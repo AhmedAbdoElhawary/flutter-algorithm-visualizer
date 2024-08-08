02ba0fba2ed1bedcd03abcd1303b7575c4d2ce45
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-08 20:15:56 +0300

    create CustomRoundedElevatedButton

---
+++ lib/core/widgets/custom_widgets/custom_rounded_elevated_button.dart
@@ -0,0 +1,48 @@
+import 'package:algorithm_visualizer/core/resources/color_manager.dart';
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomRoundedElevatedButton extends StatelessWidget {
+  final ThemeEnum? backgroundColor;
+  final ThemeEnum? shadowColor;
+  final Widget child;
+  final VoidCallback onPressed;
+  final bool fitToContent;
+  final bool smallRounded;
+  final double fixedSize;
+  const CustomRoundedElevatedButton({
+    super.key,
+    this.backgroundColor = ThemeEnum.focusColor,
+    this.shadowColor = ThemeEnum.transparentColor,
+    this.fitToContent = true,
+    this.smallRounded = false,
+    this.fixedSize = 35,
+    required this.child,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundColor = this.backgroundColor;
+    final shadowColor = this.shadowColor;
+    final background =
+        backgroundColor == null ? null : context.getColor(backgroundColor);
+    final shadow = shadowColor == null ? null : context.getColor(shadowColor);
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: background,
+        shadowColor: shadow ?? ColorManager.transparent,
+        fixedSize: fitToContent ? Size.fromHeight(fixedSize.r) : null,
+        padding: EdgeInsets.symmetric(horizontal: 15.r),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(smallRounded ? 10 : 50).r),
+        surfaceTintColor: background,
+        foregroundColor: context.getColor(ThemeEnum.hintColor),
+      ),
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+}
