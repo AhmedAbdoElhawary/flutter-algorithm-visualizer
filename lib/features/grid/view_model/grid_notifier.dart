e68d6c8ba1c33497a25337f621a4f14f3083ce94
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-09 00:06:27 +0300

    add clearTheGrid, onPointerDownOnGrid, onPointerUpOnGrid in GridNotifierCubit

--- lib/features/grid/view_model/grid_notifier.dart
+++ lib/features/grid/view_model/grid_notifier.dart
@@ -3,41 +3,53 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'grid_notifier_state.dart';
 
-final gridNotifierProvider =
-    StateNotifierProvider<GridNotifierCubit, GridNotifierState>(
-  (ref) => GridNotifierCubit(),
-);
-
 class GridNotifierCubit extends StateNotifier<GridNotifierState> {
   GridNotifierCubit() : super(GridNotifierState());
+  final int columnSquares = 20;
+  bool isTapDown = false;
 
   void updateGridLayout(BoxConstraints constraints) {
-    const int numSquares = 20;
-
     // [gridSize] it's square, so the height is the same of width. Size(value,value) => value
-    final double gridSize = (constraints.maxWidth < constraints.maxHeight)
-        ? constraints.maxWidth / numSquares
-        : constraints.maxHeight / numSquares;
+    final screenWidth = constraints.maxWidth;
+    final screenHeight = constraints.maxHeight;
 
-    final columnCrossAxisCount = (constraints.maxWidth / gridSize).floor();
-    final rowMainAxisCount = (constraints.maxHeight / gridSize).floor();
+    final double gridSize = (screenWidth < screenHeight)
+        ? screenWidth / columnSquares
+        : screenHeight / columnSquares;
+
+    final columnCrossAxisCount = (screenWidth / gridSize).floor();
+    final rowMainAxisCount = (screenHeight / gridSize).floor();
 
     final count = columnCrossAxisCount * rowMainAxisCount;
-    state = state.copyWith(
-        columnCrossAxisCount: columnCrossAxisCount,
-        rowMainAxisCount: rowMainAxisCount,
-        gridCount: count,
-        gridSize: gridSize);
-  }
-}
 
-extension GridNotifierCubitHelper on WidgetRef {
-  int get watchGridCount =>
-      watch(gridNotifierProvider.select((it) => it.gridCount));
-  int get watchColumnCrossAxisCount =>
-      watch(gridNotifierProvider.select((it) => it.columnCrossAxisCount));
-  int get watchRowMainAxisCount =>
-      watch(gridNotifierProvider.select((it) => it.rowMainAxisCount));
-  double get watchGridSize =>
-      watch(gridNotifierProvider.select((it) => it.gridSize));
+    state = state.copyWith(
+      columnCrossAxisCount: columnCrossAxisCount,
+      rowMainAxisCount: rowMainAxisCount,
+      gridCount: count,
+      gridSize: gridSize,
+      screenWidth: screenWidth,
+      screenHeight: screenHeight,
+      gridData: List<bool>.filled(count, false),
+    );
+
+    debugPrint("screen constraints updated ===========> "
+        "width: $screenWidth,,"
+        "height: $screenHeight,,");
+  }
+
+  // todo: make another way. it's not efficient for watch
+  void clearTheGrid() {
+    state = state.copyWith(
+        gridData: List<bool>.filled(state.gridCount, false),
+        currentTappedIndex: -1);
+  }
+
+  void onPointerDownOnGrid(PointerDownEvent event) {
+    isTapDown = true;
+  }
+
+  void onPointerUpOnGrid(PointerUpEvent event) {
+    isTapDown = false;
+  }
+
 }
