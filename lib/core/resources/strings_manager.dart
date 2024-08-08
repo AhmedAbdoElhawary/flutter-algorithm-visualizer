d82370f5e660311c1465072d2c57af97be9857ae
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-08 20:20:15 +0300

    create StringsManager

---
+++ lib/core/resources/strings_manager.dart
@@ -0,0 +1,23 @@
+/// [StringsManager] text in english is the same key for translation map
+library;
+
+class StringsManager {
+  static const String english = "English";
+  static const String arabic = "Arabic";
+  static const String turkish = "Turkish";
+  static const String spanish = "Spanish";
+  static const String germany = "Germany";
+  static const String french = "French";
+  static const String italian = "Italian";
+  static const String russian = "Russian";
+  static const String hindi = "Hindi";
+  static const String chinese = "Chinese";
+  static const String japanese = "Japanese";
+  static const String korean = "Korean";
+
+  /// ----------------------------------------------------------------------->
+
+  static const String appName = "Algorithm Visualizer";
+  static const String welcomeToApp = "Welcome to $appName";
+
+}
