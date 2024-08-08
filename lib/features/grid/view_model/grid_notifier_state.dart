6ea31bbeb7921d79c14031199f2fb90272a897b7
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-08 21:36:08 +0300

    add GridNotifierInitial states

--- lib/features/grid/view_model/grid_notifier_state.dart
+++ lib/features/grid/view_model/grid_notifier_state.dart
@@ -1,6 +1,28 @@
-
 part of 'grid_notifier.dart';
 
-final class GridNotifierInitial  {
+final class GridNotifierInitial {
+  final int columnCrossAxisCount;
+  final int rowMainAxisCount;
+  final int gridCount;
+  final int gridSize;
 
+  GridNotifierInitial({
+    required this.columnCrossAxisCount,
+    required this.rowMainAxisCount,
+    required this.gridCount,
+    required this.gridSize,
+  });
+  GridNotifierInitial copyWith({
+    int? columnCrossAxisCount,
+    int? rowMainAxisCount,
+    int? gridCount,
+    int? gridSize,
+  }) {
+    return GridNotifierInitial(
+      columnCrossAxisCount: columnCrossAxisCount ?? this.columnCrossAxisCount,
+      rowMainAxisCount: rowMainAxisCount ?? this.rowMainAxisCount,
+      gridSize: gridSize ?? this.gridSize,
+      gridCount: gridCount ?? this.gridCount,
+    );
+  }
 }
