e43a84dadf2901294e219512e00c1c628ad47775
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-08 22:36:36 +0300

    add updateGridLayout, GridNotifierCubitHelper in GridNotifierCubit

--- lib/features/grid/view_model/grid_notifier.dart
+++ lib/features/grid/view_model/grid_notifier.dart
@@ -1,8 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'grid_notifier_state.dart';
 
-class GridNotifierCubit extends StateNotifier<GridNotifierInitial> {
-  GridNotifierCubit() : super(GridNotifierInitial());
+final gridNotifierProvider =
+    StateNotifierProvider<GridNotifierCubit, GridNotifierState>(
+  (ref) => GridNotifierCubit(),
+);
 
+class GridNotifierCubit extends StateNotifier<GridNotifierState> {
+  GridNotifierCubit() : super(GridNotifierState());
+
+  void updateGridLayout(BoxConstraints constraints) {
+    const int numSquares = 20;
+
+    // [gridSize] it's square, so the height is the same of width. Size(value,value) => value
+    final double gridSize = (constraints.maxWidth < constraints.maxHeight)
+        ? constraints.maxWidth / numSquares
+        : constraints.maxHeight / numSquares;
+
+    final columnCrossAxisCount = (constraints.maxWidth / gridSize).floor();
+    final rowMainAxisCount = (constraints.maxHeight / gridSize).floor();
+
+    final count = columnCrossAxisCount * rowMainAxisCount;
+    state = state.copyWith(
+        columnCrossAxisCount: columnCrossAxisCount,
+        rowMainAxisCount: rowMainAxisCount,
+        gridCount: count,
+        gridSize: gridSize);
+  }
+}
+
+extension GridNotifierCubitHelper on WidgetRef {
+  int get watchGridCount =>
+      watch(gridNotifierProvider.select((it) => it.gridCount));
+  int get watchColumnCrossAxisCount =>
+      watch(gridNotifierProvider.select((it) => it.columnCrossAxisCount));
+  int get watchRowMainAxisCount =>
+      watch(gridNotifierProvider.select((it) => it.rowMainAxisCount));
+  double get watchGridSize =>
+      watch(gridNotifierProvider.select((it) => it.gridSize));
 }
