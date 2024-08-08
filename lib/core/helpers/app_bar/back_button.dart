f152c362788c26b28bb7d8c0a584b36f2aa0dfa2
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-08 20:44:04 +0300

    change to default BackButtonIcon in CustomBackButtonIcon

--- lib/core/helpers/app_bar/back_button.dart
+++ lib/core/helpers/app_bar/back_button.dart
@@ -1,7 +1,3 @@
-import 'package:algorithm_visualizer/core/enums/app_settings_enum.dart';
-import 'package:algorithm_visualizer/core/helpers/storage/app_settings/app_settings_cubit.dart';
-import 'package:algorithm_visualizer/core/helpers/svg_picture.dart';
-import 'package:algorithm_visualizer/core/resources/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,23 +21,6 @@ class CustomBackButtonIcon extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final isLangEnglish =
-        ref.watch(appSettingsProvider).language == LanguagesEnum.english;
-
-    final String icon;
-    switch (Theme.of(context).platform) {
-      case TargetPlatform.android:
-      case TargetPlatform.fuchsia:
-      case TargetPlatform.linux:
-      case TargetPlatform.windows:
-        icon =
-            isLangEnglish ? IconsAssets.backButton : IconsAssets.backButtonRTL;
-      case TargetPlatform.iOS:
-      case TargetPlatform.macOS:
-        icon = isLangEnglish
-            ? IconsAssets.iosBackButton
-            : IconsAssets.iosBackButtonRTL;
-    }
-    return CustomAssetsSvg(icon, size: 30);
+    return const BackButtonIcon();
   }
 }
