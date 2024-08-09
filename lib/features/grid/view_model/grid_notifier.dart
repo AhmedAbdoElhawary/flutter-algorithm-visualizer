828cee89e34534c7fd8427dea4c3c00a7590aaf3
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-09 03:20:27 +0300

    add _addDefaultTwoPoints in (start, target)GridNotifierCubit

--- lib/features/grid/view_model/grid_notifier.dart
+++ lib/features/grid/view_model/grid_notifier.dart
@@ -9,7 +9,6 @@ class GridNotifierCubit extends StateNotifier<GridNotifierState> {
   bool isTapDown = false;
 
   void updateGridLayout(BoxConstraints constraints) {
-    // [gridSize] it's square, so the height is the same of width. Size(value,value) => value
     final screenWidth = constraints.maxWidth;
     final screenHeight = constraints.maxHeight;
 
@@ -22,26 +21,80 @@ class GridNotifierCubit extends StateNotifier<GridNotifierState> {
 
     final count = columnCrossAxisCount * rowMainAxisCount;
 
-    state = state.copyWith(
+    final tempState = state.copyWith(
       columnCrossAxisCount: columnCrossAxisCount,
       rowMainAxisCount: rowMainAxisCount,
       gridCount: count,
       gridSize: gridSize,
       screenWidth: screenWidth,
       screenHeight: screenHeight,
-      gridData: List<bool>.filled(count, false),
     );
+
+    final gridData = _addDefaultTwoPoints(addState: tempState);
+
+    state = tempState.copyWith(gridData: gridData);
 
     debugPrint("screen constraints updated ===========> "
         "width: $screenWidth,,"
         "height: $screenHeight,,");
   }
 
+  List<GridStatus> _addDefaultTwoPoints({required GridNotifierState addState}) {
+    final gridData =
+        List<GridStatus>.filled(addState.gridCount, GridStatus.empty);
+
+    final isHeightTaller = addState.screenHeight > addState.screenWidth;
+
+    return isHeightTaller
+        ? _twoPointersForHeight(addState: addState, gridStatus: gridData)
+        : _twoPointersForWidth(addState: addState, gridStatus: gridData);
+  }
+
+  List<GridStatus> _twoPointersForHeight({
+    required List<GridStatus> gridStatus,
+    required GridNotifierState addState,
+  }) {
+    final centerColumn = addState.columnCrossAxisCount ~/ 2;
+
+    final topRow = addState.rowMainAxisCount ~/ 8;
+    final bottomRow = addState.rowMainAxisCount ~/ 1.2;
+
+    final topCenterIndex =
+        (topRow) * addState.columnCrossAxisCount + centerColumn;
+
+    final bottomCenterIndex =
+        (bottomRow) * addState.columnCrossAxisCount + centerColumn;
+
+    gridStatus[topCenterIndex] = GridStatus.startPoint;
+    gridStatus[bottomCenterIndex] = GridStatus.targetPoint;
+    return gridStatus;
+  }
+
+  List<GridStatus> _twoPointersForWidth({
+    required List<GridStatus> gridStatus,
+    required GridNotifierState addState,
+  }) {
+    final centerRow = addState.rowMainAxisCount ~/ 2;
+
+    final rightRow = addState.columnCrossAxisCount ~/ 1.2;
+    final leftRow = addState.columnCrossAxisCount ~/ 8;
+
+    final leftCenterIndex =
+        centerRow * addState.columnCrossAxisCount + (leftRow);
+    final rightCenterIndex =
+        centerRow * addState.columnCrossAxisCount + (rightRow);
+
+    gridStatus[leftCenterIndex] = GridStatus.startPoint;
+    gridStatus[rightCenterIndex] = GridStatus.targetPoint;
+
+    return gridStatus;
+  }
+
   // todo: make another way. it's not efficient for watch
   void clearTheGrid() {
-    state = state.copyWith(
-        gridData: List<bool>.filled(state.gridCount, false),
-        currentTappedIndex: -1);
+    final grids = _addDefaultTwoPoints(addState: state);
+
+    state = state.copyWith(gridData: grids, currentTappedIndex: -1);
   }
 
   void onPointerDownOnGrid(PointerDownEvent event) {
@@ -52,24 +105,39 @@ class GridNotifierCubit extends StateNotifier<GridNotifierState> {
     isTapDown = false;
   }
 
-  void onPointerMoveOnGrid(PointerMoveEvent event) {
+  void onFingerMoveOnGrid(PointerMoveEvent event) {
     final dx = event.localPosition.dx;
     final dy = event.localPosition.dy;
-
-    final selectedColumn = (dx / state.gridSize).floor();
-    final selectedRow = (dy / state.gridSize).floor();
-
-    final index = selectedRow * state.columnCrossAxisCount + selectedColumn;
+    final index = _getIndex(addState: state, screenHeight: dy, screenWidth: dx);
 
     // to handle multi calls from listener widget
     if (index == state.currentTappedIndex) return;
 
     if (index >= 0 && index < state.gridData.length) {
-      final updatedGridData = List<bool>.from(state.gridData);
-      updatedGridData[index] = !updatedGridData[index];
+      final updatedGridData = List<GridStatus>.from(state.gridData);
+      final currentGrid = updatedGridData[index];
+      if (currentGrid == GridStatus.wall) {
+        updatedGridData[index] = GridStatus.empty;
+      } else if (currentGrid == GridStatus.empty) {
+        updatedGridData[index] = GridStatus.wall;
+      }
+      // = !updatedGridData[index];
 
       state =
           state.copyWith(gridData: updatedGridData, currentTappedIndex: index);
     }
+  }
+
+  int _getIndex({
+    required GridNotifierState addState,
+    required double screenWidth,
+    required double screenHeight,
+  }) {
+    final selectedColumn = (screenWidth / addState.gridSize).floor();
+    final selectedRow = (screenHeight / addState.gridSize).floor();
+
+    final index = selectedRow * addState.columnCrossAxisCount + selectedColumn;
+
+    return index;
   }
 }
