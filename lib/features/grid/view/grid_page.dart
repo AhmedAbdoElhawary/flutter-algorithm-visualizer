12164260381b3f6f631f1a2525a1d64f359f16ad
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-09 03:19:19 +0300

    add different grid status in UI

--- lib/features/grid/view/grid_page.dart
+++ lib/features/grid/view/grid_page.dart
@@ -1,3 +1,4 @@
+import 'package:algorithm_visualizer/core/resources/color_manager.dart';
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/features/grid/view_model/grid_notifier.dart';
@@ -95,7 +96,7 @@ class _BuildGridItems extends ConsumerWidget {
 
     return Listener(
       onPointerDown: read.onPointerDownOnGrid,
-      onPointerMove: read.onPointerMoveOnGrid,
+      onPointerMove: read.onFingerMoveOnGrid,
       onPointerUp: read.onPointerUpOnGrid,
       child: GridView.builder(
         physics: const NeverScrollableScrollPhysics(),
@@ -126,33 +127,138 @@ class _SquareState extends ConsumerState<_Square> {
     final isSelected = ref
         .watch(gridNotifierProvider.select((it) => it.gridData[widget.index]));
 
-    final isBlack = isSelected;
+    final isColored = isSelected != GridStatus.empty;
 
     return Container(
       decoration: BoxDecoration(
-        border: isBlack
-            ? Border.symmetric(
-                vertical:
-                    BorderSide(color: Colors.grey.withOpacity(0.5), width: 0.3))
-            : Border.all(color: Colors.grey.withOpacity(0.5), width: 0.5),
+        border: isColored ? _thineVerticalBorder() : _allBorder(),
       ),
       child: AnimatedScale(
-        scale: isBlack ? 1.0 : 0.1,
+        scale: isColored ? 1.0 : 0.1,
         duration: const Duration(milliseconds: 700),
         curve: Curves.elasticOut,
-        child: Consumer(
-          builder: (context, ref, _) {
-            final size =
-                ref.watch(gridNotifierProvider.select((it) => it.gridSize));
-
-            return Container(
-              width: size,
-              height: size,
-              color: isBlack ? Colors.black : Colors.transparent,
-            );
+        child: Builder(
+          builder: (context) {
+            switch (isSelected) {
+              case GridStatus.wall:
+                return const _WallGrid();
+              case GridStatus.startPoint:
+                return const _StartPointGrid();
+              case GridStatus.targetPoint:
+                return const _TargetPointGrid();
+              case GridStatus.searcher:
+                return const _SearcherGrid();
+              default:
+                return const _DefaultGrid();
+            }
           },
         ),
       ),
     );
+  }
+
+  Border _allBorder() =>
+      Border.all(color: ColorManager.grey.withOpacity(0.5), width: 0.5);
+
+  Border _thineVerticalBorder() {
+    return Border.symmetric(
+        vertical:
+            BorderSide(color: ColorManager.grey.withOpacity(0.5), width: 0.3));
+  }
+}
+
+class _WallGrid extends StatelessWidget {
+  const _WallGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, _) {
+        final size =
+            ref.watch(gridNotifierProvider.select((it) => it.gridSize));
+
+        return Container(
+          width: size,
+          height: size,
+          decoration: const BoxDecoration(color: ColorManager.black),
+        );
+      },
+    );
+  }
+}
+
+class _StartPointGrid extends StatelessWidget {
+  const _StartPointGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final size = ref.watch(gridNotifierProvider.select((it) => it.gridSize));
+
+      return Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+            color: ColorManager.yellow.withOpacity(.4), shape: BoxShape.circle),
+        // padding: REdgeInsets.all(2),
+        child: const FittedBox(child: Icon(Icons.arrow_forward)),
+      );
+    });
+  }
+}
+
+class _TargetPointGrid extends StatelessWidget {
+  const _TargetPointGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final size = ref.watch(gridNotifierProvider.select((it) => it.gridSize));
+
+      return Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+            color: ColorManager.red.withOpacity(.4), shape: BoxShape.circle),
+        // padding: REdgeInsets.all(),
+        child: const FittedBox(child: Icon(Icons.control_point_rounded)),
+      );
+    });
+  }
+}
+
+class _SearcherGrid extends StatelessWidget {
+  const _SearcherGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final size = ref.watch(gridNotifierProvider.select((it) => it.gridSize));
+
+      return Container(
+        width: size,
+        height: size,
+        decoration: const BoxDecoration(color: ColorManager.orange),
+        // padding: REdgeInsets.all(2),
+        // child: const Icon(Icons.control_point_rounded),
+      );
+    });
+  }
+}
+
+class _DefaultGrid extends StatelessWidget {
+  const _DefaultGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final size = ref.watch(gridNotifierProvider.select((it) => it.gridSize));
+
+      return Container(
+        width: size,
+        height: size,
+        decoration: const BoxDecoration(color: ColorManager.transparent),
+      );
+    });
   }
 }
