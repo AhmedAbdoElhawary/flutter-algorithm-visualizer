e4fa1a959c40935ef7b4397d9be038ba798547b4
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-09 21:47:26 +0300

    separate _SearcherGrid and change some colors

--- lib/features/grid/view/grid_page.dart
+++ lib/features/grid/view/grid_page.dart
@@ -1,14 +1,25 @@
 import 'package:algorithm_visualizer/core/resources/color_manager.dart';
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
+import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
 import 'package:algorithm_visualizer/features/grid/view_model/grid_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+part '../widgets/searcher_grid.dart';
 
 final gridNotifierProvider =
     StateNotifierProvider<GridNotifierCubit, GridNotifierState>(
   (ref) => GridNotifierCubit(),
 );
+
+Border _allBorder() =>
+    Border.all(color: ColorManager.dividerBlue, width: 0.5.r);
+
+Border _thineVerticalBorder() {
+  return Border.symmetric(
+      vertical: BorderSide(color: ColorManager.white, width: 0.5.r));
+}
 
 class GridPage extends StatelessWidget {
   const GridPage({super.key});
@@ -156,15 +167,6 @@ class _SquareState extends ConsumerState<_Square> {
       ),
     );
   }
-
-  Border _allBorder() =>
-      Border.all(color: ColorManager.grey.withOpacity(0.5), width: 0.5);
-
-  Border _thineVerticalBorder() {
-    return Border.symmetric(
-        vertical:
-            BorderSide(color: ColorManager.grey.withOpacity(0.5), width: 0.3));
-  }
 }
 
 class _WallGrid extends StatelessWidget {
@@ -180,7 +182,7 @@ class _WallGrid extends StatelessWidget {
         return Container(
           width: size,
           height: size,
-          decoration: const BoxDecoration(color: ColorManager.black),
+          decoration: const BoxDecoration(color: ColorManager.wallBlack),
         );
       },
     );
@@ -200,11 +202,14 @@ class _StartPointGrid extends StatelessWidget {
         return Container(
           width: size,
           height: size,
-          decoration: BoxDecoration(
-              color: ColorManager.yellow.withOpacity(.4),
-              shape: BoxShape.circle),
-          // padding: REdgeInsets.all(2),
-          child: const FittedBox(child: Icon(Icons.arrow_forward)),
+          decoration: const BoxDecoration(shape: BoxShape.circle),
+          child:  const FittedBox(
+            child: CustomIcon(
+              Icons.arrow_forward_ios_rounded,
+              size: 50,
+              color: ColorManager.darkPurple,
+            ),
+          ),
         );
       },
     );
@@ -224,115 +229,40 @@ class _TargetPointGrid extends StatelessWidget {
         return Container(
           width: size,
           height: size,
-          decoration: BoxDecoration(
-              color: ColorManager.red.withOpacity(.4), shape: BoxShape.circle),
-          // padding: REdgeInsets.all(),
-          child: const FittedBox(child: Icon(Icons.control_point_rounded)),
+          decoration: const BoxDecoration(shape: BoxShape.circle),
+          child: const FittedBox(
+            child: _Circle(
+              radius: 30,
+              backgroundColor: ColorManager.darkPurple,
+              child: _Circle(
+                radius: 20,
+                backgroundColor: ColorManager.white,
+                child: _Circle(radius: 12, backgroundColor: ColorManager.darkPurple),
+              ),
+            ),
+          ),
         );
       },
     );
   }
 }
 
-class _SearcherGrid extends StatefulWidget {
-  const _SearcherGrid();
-
-  @override
-  State<_SearcherGrid> createState() => _SearcherGridState();
-}
-
-class _SearcherGridState extends State<_SearcherGrid>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<Color?> _colorAnimation;
-  late Animation<double> _shapeAnimation;
-
-  final startColor = ColorManager.darkBlue;
-  final mediumColor = ColorManager.mediumBlue;
-  final finishedSearcherColor = ColorManager.finishedSearcherBlue;
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration:
-          const Duration(milliseconds: 1500), // Total duration of the animation
-      vsync: this,
-    );
-
-    // Define the scale animation from 0.1 to 1.2 then back to 1.0
-    _scaleAnimation = TweenSequence([
-      TweenSequenceItem(tween: Tween<double>(begin: 0.1, end: 1.4), weight: 60),
-      TweenSequenceItem(tween: Tween<double>(begin: 1.4, end: 1.0), weight: 40),
-    ]).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    // Define the color animation
-    _colorAnimation = TweenSequence([
-      TweenSequenceItem(
-          tween: ColorTween(begin: startColor, end: mediumColor), weight: 40),
-      TweenSequenceItem(
-          tween: ColorTween(begin: mediumColor, end: finishedSearcherColor),
-          weight: 60),
-    ]).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.3, 0.8, curve: Curves.easeInOut),
-      ),
-    );
-
-    //                             (begin => circle, end => rectangle)
-    _shapeAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.1, 0.65, curve: Curves.easeInOut),
-      ),
-    );
-
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.reverse();
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _Circle extends StatelessWidget {
+  const _Circle({
+    this.child,
+    required this.radius,
+    required this.backgroundColor,
+  });
+  final Widget? child;
+  final double radius;
+  final Color backgroundColor;
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (context, ref, _) {
-      final size = ref.watch(gridNotifierProvider.select((it) => it.gridSize));
-
-      return AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          return Transform.scale(
-            scale: _scaleAnimation.value,
-            child: Container(
-              width: size,
-              height: size,
-              decoration: BoxDecoration(
-                border: Border.symmetric(
-                    vertical: BorderSide(
-                        color: ColorManager.grey.withOpacity(0.5), width: 0.3)),
-                borderRadius:
-                    BorderRadius.circular(_shapeAnimation.value * (size / 2)),
-                color: _controller.value < 0.4
-                    ? Color.lerp(ColorManager.transparent, startColor,
-                        _controller.value / 0.4)
-                    : _colorAnimation.value,
-              ),
-            ),
-          );
-        },
-      );
-    });
+    return CircleAvatar(
+      radius: radius.r,
+      backgroundColor: backgroundColor,
+      child: child,
+    );
   }
 }
 
