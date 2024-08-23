bc811c466c2a4f36814ddf55e393b5d603d64322
ahmed <ahmedabdoelhawari12345work@gmail.com>
2024-08-23 18:48:22 +0300

    create CustomRandom

---
+++ lib/core/helpers/random_int.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+class CustomRandom {
+  static List<int> generateRandomList(int maxNum, int length) {
+    if (length > maxNum + 1) length = maxNum;
+
+    List<int> numbers = List<int>.generate(maxNum + 1, (index) => index);
+
+    numbers.shuffle(Random());
+
+    return numbers.sublist(0, length);
+  }
+}
