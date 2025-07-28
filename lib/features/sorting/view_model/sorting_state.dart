969184f2a9f1dab0f99c7a3e17eefe657962bb3c
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-07-28 21:44:53 +0300

    handle SortingNotifierState

--- lib/features/sorting/view_model/sorting_state.dart
+++ lib/features/sorting/view_model/sorting_state.dart
@@ -1,18 +1,26 @@
 part of 'sorting_notifier.dart';
 
 class SortingNotifierState {
+  final ComparableTwoItems? comparableTwoItems;
   final List<SortableItem> list;
   final Map<int, Offset> positions;
   final List<SortingAlgorithm> selectedAlgorithms;
 
-  SortingNotifierState({required this.list, this.positions = const {}, this.selectedAlgorithms = const []});
+  SortingNotifierState({
+    this.comparableTwoItems,
+    required this.list,
+    this.positions = const {},
+    this.selectedAlgorithms = const [],
+  });
 
   SortingNotifierState copyWith({
+    ComparableTwoItems? comparableTwoItems,
     List<SortableItem>? list,
     Map<int, Offset>? positions,
     List<SortingAlgorithm>? selectedAlgorithms,
   }) {
     return SortingNotifierState(
+      comparableTwoItems: comparableTwoItems ?? this.comparableTwoItems,
       list: list ?? this.list,
       positions: positions ?? this.positions,
       selectedAlgorithms: selectedAlgorithms ?? this.selectedAlgorithms,
