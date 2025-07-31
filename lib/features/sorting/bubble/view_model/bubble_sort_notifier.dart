213c7c1002a4ad7ea033acd8e017a1e0f84362c5
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-07-31 16:40:08 +0300

    cleanup sorting logic

--- lib/features/sorting/bubble/view_model/bubble_sort_notifier.dart
+++ lib/features/sorting/bubble/view_model/bubble_sort_notifier.dart
@@ -1,66 +1,8 @@
 import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:collection/collection.dart';
-import 'package:flutter/material.dart';
 
 class BubbleSortNotifier extends SortingNotifier {
   @override
-  Future<void> buildSort() async {
-    final list = List<SortableItem>.from(state.list);
-    final values = list.map((e) => e.value).toList();
-
-    final steps = bubbleSortSteps(values);
-
-    for (final step in steps) {
-      if (operation != SortingEnum.played) return;
-
-      switch (step.action) {
-        case SortingStatus.compared:
-          list[step.index1] = list[step.index1].copyWith(sortedStatus: SortingStatus.compared);
-          list[step.index2] = list[step.index2].copyWith(sortedStatus: SortingStatus.compared);
-          state = state.copyWith(list: list);
-
-          await Future.delayed(speedDuration);
-
-          break;
-
-        case SortingStatus.swapped:
-          list[step.index1] = list[step.index1].copyWith(sortedStatus: SortingStatus.swapped);
-          list[step.index2] = list[step.index2].copyWith(sortedStatus: SortingStatus.swapped);
-          state = state.copyWith(list: list);
-
-          await Future.delayed(speedDuration);
-
-          list.swap(step.index1, step.index2);
-
-          final positions = Map<int, Offset>.from(state.positions);
-          final tempPosition = positions[list[step.index1].id]!;
-          positions[list[step.index1].id] = positions[list[step.index2].id]!;
-          positions[list[step.index2].id] = tempPosition;
-
-          state = state.copyWith(list: list, positions: positions);
-          break;
-
-        case SortingStatus.unSorted:
-          list[step.index1] = list[step.index1].copyWith(sortedStatus: SortingStatus.unSorted);
-          list[step.index2] = list[step.index2].copyWith(sortedStatus: SortingStatus.unSorted);
-          state = state.copyWith(list: list);
-          break;
-
-        // i don't want to make it green while sorting and mark all of them at once as green at the end
-        case SortingStatus.sorted:
-        case SortingStatus.none:
-          list[step.index1] = list[step.index1].copyWith(sortedStatus: SortingStatus.none);
-          state = state.copyWith(list: list);
-          break;
-      }
-
-      await Future.delayed(speedDuration);
-    }
-
-    await greenSortedItemsAsDone();
-  }
-
-  List<SortingStep> bubbleSortSteps(List<int> values) {
+  List<SortingStep> buildSorting(List<int> values) {
     final steps = <SortingStep>[];
     final arr = List<int>.from(values);
 
