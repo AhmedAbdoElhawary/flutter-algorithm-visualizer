74dec7af69c7b688e4e424b44c404d71288f15da
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-07-31 15:22:52 +0300

    create MergeSortNotifier

---
+++ lib/features/sorting/merge/view_model/merge_sort_notifier.dart
@@ -0,0 +1,8 @@
+import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
+
+class MergeSortNotifier extends SortingNotifier {
+  @override
+  Future<void> buildSort() async {
+    /// TODO: implement this
+  }
+}
