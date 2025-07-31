9e02a50c06db3ad3c2733c0b70ccaa118b69337d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-07-31 14:44:59 +0300

    create SortingListPage

---
+++ lib/features/sorting/base/view/sorting_list_page.dart
@@ -0,0 +1,46 @@
+import 'package:algorithm_visualizer/config/routes/route_app.dart';
+import 'package:algorithm_visualizer/core/extensions/navigators.dart';
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
+import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_rounded_elevated_button.dart';
+import 'package:flutter/material.dart';
+
+class SortingListPage extends StatelessWidget {
+  const SortingListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: const InkWell(child: RegularText(StringsManager.sorting)),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomRoundedElevatedButton(
+                roundedRadius: 3,
+                backgroundColor: ThemeEnum.whiteD5Color,
+                child: const RegularText(StringsManager.bubbleSort),
+                onPressed: () {
+                  context.pushTo(Routes.bubbleSort);
+                },
+              ),
+              CustomRoundedElevatedButton(
+                roundedRadius: 3,
+                backgroundColor: ThemeEnum.whiteD5Color,
+                child: const RegularText(StringsManager.selectionSort),
+                onPressed: () {
+                  // context.pushTo(Routes.sorting);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
