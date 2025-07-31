8f8d07de2838df5e212c2340ead7c3cf4569c987
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-07-31 16:41:27 +0300

    move other notifier for new logic

--- lib/features/sorting/merge/view_model/merge_sort_notifier.dart
+++ lib/features/sorting/merge/view_model/merge_sort_notifier.dart
@@ -2,7 +2,8 @@ import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_no
 
 class MergeSortNotifier extends SortingNotifier {
   @override
-  Future<void> buildSort() async {
-    /// TODO: implement this
+  List<SortingStep> buildSorting(List<int> values) {
+    // TODO: implement buildSorting
+    throw UnimplementedError();
   }
 }
