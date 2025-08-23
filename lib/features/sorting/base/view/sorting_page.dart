b4c2384a309b627bb01664c2c1891824b89f93c4
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-23 20:59:12 +0300

    refactoring and handle size changing while running

--- lib/features/sorting/base/view/sorting_page.dart
+++ lib/features/sorting/base/view/sorting_page.dart
@@ -1,6 +1,8 @@
 import 'package:algorithm_visualizer/core/draggable_progress.dart' show DraggableProgressBar;
+import 'package:algorithm_visualizer/core/resources/color_manager.dart';
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:algorithm_visualizer/core/widgets/adaptive/padding/adaptive_padding.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_rounded_elevated_button.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
@@ -10,12 +12,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 part '../widgets/sorting_app_bar.dart';
 part '../widgets/control_buttons.dart';
 
-class SortingPage extends ConsumerWidget {
+class SortingPage extends StatelessWidget {
   const SortingPage({required this.instance, required this.title, super.key});
   final StateNotifierProvider<SortingNotifier, SortingNotifierState> instance;
   final String title;
   @override
-  Widget build(BuildContext context, ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
       body: SafeArea(
@@ -33,19 +35,15 @@ class SortingPage extends ConsumerWidget {
                   spacing: 10,
                   children: [
                     _ControlButtons(instance),
-                    Row(
-                      children: [
-                        DraggableProgressBar(
-                          onChanged: (persent) {
-                            ref.read(instance.notifier).changeSpeed(persent);
-                          },
-                        ),
-                        DraggableProgressBar(
-                          onChanged: (persent) {
-                            ref.read(instance.notifier).changeSize(persent);
-                          },
-                        ),
-                      ],
+                    SymmetricPadding(
+                      horizontal: 15,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SpeedDraggable(instance: instance),
+                          SizeDraggable(instance: instance),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -64,6 +62,75 @@ class SortingPage extends ConsumerWidget {
         builder: (context, ref, _) {
           return InkWell(child: RegularText(title));
         },
+      ),
+    );
+  }
+}
+
+class SpeedDraggable extends ConsumerWidget {
+  const SpeedDraggable({super.key, required this.instance});
+
+  final StateNotifierProvider<SortingNotifier, SortingNotifierState> instance;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Flexible(
+      child: Row(
+        children: [
+          const MediumText(StringsManager.speed, fontSize: 12),
+          Flexible(
+            child: DraggableProgressBar(
+              runOnChangedInitially: true,
+              onChanged: (persent) {
+                ref.read(instance.notifier).changeSpeed(persent);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SizeDraggable extends ConsumerWidget {
+  const SizeDraggable({super.key, required this.instance});
+
+  final StateNotifierProvider<SortingNotifier, SortingNotifierState> instance;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final operationStatus = ref.watch(instance.select((state) => state.operationStatus));
+    final isRunning = operationStatus == SortingEnum.played || operationStatus == SortingEnum.stopped;
+    return Flexible(
+      child: Row(
+        children: [
+          MediumText(
+            StringsManager.size,
+            fontSize: 12,
+            color: isRunning ? ThemeEnum.whiteD7Color : null,
+          ),
+          Flexible(
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                DraggableProgressBar(
+                  isActive: !isRunning,
+                  runOnChangedInitially: true,
+                  sliderValue: 0.15,
+                  onChanged: (persent) {
+                    ref.read(instance.notifier).changeSize(persent);
+                  },
+                ),
+                if (isRunning)
+                  Container(
+                    color: ColorManager.transparent,
+                    height: 20.r,
+                    width: double.infinity,
+                  )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -95,8 +162,8 @@ class _BuildList extends ConsumerWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 5),
-      child: SizedBox(
-        height: SortingNotifier.maxListItemHeight * 1.3,
+      child: RSizedBox(
+        height: SortingNotifier.maxListItemHeight * 1.05,
         width: double.infinity,
         child: Stack(
           alignment: AlignmentDirectional.bottomCenter,
@@ -104,11 +171,11 @@ class _BuildList extends ConsumerWidget {
             items.length,
             (index) {
               final item = items[index];
-              final position = ref.watch(instance.select((state) => state.positions[item.id]!));
+              final position = ref.watch(instance.select((state) => state.positions[item.id]));
               return AnimatedPositioned(
                 key: ValueKey(item.id),
-                left: position.dx,
-                bottom: position.dy,
+                left: position?.dx,
+                bottom: position?.dy,
                 duration: speedDuration,
                 child: _BuildItem(
                     item: item, index: index, speedDuration: speedDuration * 0.5, instance: instance),
@@ -138,7 +205,8 @@ class _BuildItem extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final size = ref.watch(instance.select((state) => state.size));
     final itemWidth = SortingNotifier.calculateItemWidth(context, size);
-    final currentItem = ref.watch(instance.select((state) => state.list[index]));
+    final currentItem =
+        ref.watch(instance.select((state) => index < state.list.length ? state.list[index] : null));
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: SortingNotifier.itemsPadding / 2),
@@ -147,7 +215,7 @@ class _BuildItem extends ConsumerWidget {
         height: SortingNotifier.calculateItemHeight(item.value, size),
         width: itemWidth,
         decoration: BoxDecoration(
-          color: context.getColor(currentItem.getColor),
+          color: context.getColor(currentItem?.getColor ?? SortingNotifier.itemColor),
           borderRadius: const BorderRadius.vertical(top: Radius.circular(1)),
         ),
       ),
