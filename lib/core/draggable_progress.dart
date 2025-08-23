070bf264e793707b2ef598b64d8884d35d7493dc
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-23 20:58:23 +0300

    handle DraggableProgressBar

--- lib/core/draggable_progress.dart
+++ lib/core/draggable_progress.dart
@@ -2,19 +2,37 @@ import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class DraggableProgressBar extends StatefulWidget {
-  const DraggableProgressBar({required this.onChanged, super.key});
+  const DraggableProgressBar({
+    this.runOnChangedInitially = false,
+    required this.onChanged,
+    this.sliderValue = 0.3,
+    this.isActive = true,
+    super.key,
+  });
   final void Function(double persent) onChanged;
+  final bool runOnChangedInitially;
+  final double sliderValue;
+  final bool isActive;
   @override
   LinearSliderState createState() => LinearSliderState();
 }
 
 class LinearSliderState extends State<DraggableProgressBar> {
-  double sliderValue = 0.3;
+  late double sliderValue = widget.sliderValue;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (widget.runOnChangedInitially) widget.onChanged(sliderValue);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Slider(
-      activeColor: context.getColor(ThemeEnum.blueColor),
+      activeColor: widget.isActive
+          ? context.getColor(ThemeEnum.mediumBlueColor)
+          : context.getColor(ThemeEnum.whiteD7Color),
       value: sliderValue,
       onChanged: (v) {
         setState(() {
