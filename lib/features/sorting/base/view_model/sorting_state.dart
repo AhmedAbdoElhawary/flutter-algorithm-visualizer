0031397f742f4bd263822bdaa5120faa05468456
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-23 20:59:20 +0300

    refactoring and handle size changing while running

--- lib/features/sorting/base/view_model/sorting_state.dart
+++ lib/features/sorting/base/view_model/sorting_state.dart
@@ -5,7 +5,9 @@ class SortingNotifierState {
   final Map<int, Offset> positions;
   final Duration swipeDuration;
   final int size;
+  final SortingEnum operationStatus;
   SortingNotifierState({
+    this.operationStatus = SortingEnum.none,
     this.size = SortingNotifier._defaultSize,
     this.swipeDuration = SortingNotifier._defaultSpeedDuration,
     required this.list,
@@ -18,8 +20,10 @@ class SortingNotifierState {
     List<SortableItem>? list,
     Map<int, Offset>? positions,
     List<SortingAlgorithm>? selectedAlgorithms,
+    SortingEnum? operationStatus,
   }) {
     return SortingNotifierState(
+      operationStatus: operationStatus ?? this.operationStatus,
       size: size ?? this.size,
       swipeDuration: swipeDuration ?? this.swipeDuration,
       list: list ?? this.list,
