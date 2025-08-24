a42d2bff90c50d3a55c3ccccdec1349604dd531f
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-24 14:21:45 +0300

    create HeapSortPage

---
+++ lib/features/sorting/heap/view/heap_sort_page.dart
@@ -0,0 +1,30 @@
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/heap/view_model/heap_sort_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
+  (ref) => HeapSortNotifier(),
+);
+
+class HeapSortPage extends ConsumerStatefulWidget {
+  const HeapSortPage({super.key});
+
+  @override
+  ConsumerState<HeapSortPage> createState() => _HeapSortPageState();
+}
+
+class _HeapSortPageState extends ConsumerState<HeapSortPage> {
+  @override
+  void deactivate() {
+    ref.invalidate(_notifierProvider); // deletes current instance and resets
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SortingPage(instance: _notifierProvider, title: StringsManager.heapSort);
+  }
+}
