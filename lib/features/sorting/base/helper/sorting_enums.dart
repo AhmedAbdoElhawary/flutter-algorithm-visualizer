2d41143c838707d7b0a23e35395075be3c8d0462
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-08-27 15:39:49 +0300

    reformat the code

--- lib/features/sorting/base/helper/sorting_enums.dart
+++ lib/features/sorting/base/helper/sorting_enums.dart
@@ -1,18 +1,5 @@
 part of '../view_model/sorting_notifier.dart';
 
-enum SortingAlgorithm {
-  bubble,
-  selection,
-  insertion,
-  merge,
-  // quick,
-  // shell,
-  // heap,
-  // count,
-  // radix,
-  // bucket,
-}
-
 enum SortingEnum { played, stopped, none }
 
 enum SortingStatus { unSorted, compared, swapping, swapped, sorted, none }
