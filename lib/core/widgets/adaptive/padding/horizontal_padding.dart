d24ab87e19032260eab63ff764155da81c3adb3d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2025-09-17 21:37:30 +0300

    reformat the code

--- lib/core/widgets/adaptive/padding/horizontal_padding.dart
+++ lib/core/widgets/adaptive/padding/horizontal_padding.dart
@@ -1,13 +1,11 @@
 part of '../../../../core/widgets/adaptive/padding/adaptive_padding.dart';
 
 class HorizontalPadding extends StatelessWidget {
-  const HorizontalPadding(
-      {required this.padding, required this.child, super.key});
+  const HorizontalPadding({required this.padding, required this.child, super.key});
   final double padding;
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return _RPadding(
-        padding: REdgeInsets.symmetric(horizontal: padding), child: child);
+    return _RPadding(padding: REdgeInsets.symmetric(horizontal: padding), child: child);
   }
 }
