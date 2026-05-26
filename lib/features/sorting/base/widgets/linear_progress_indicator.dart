178afde16c20de440ac30305cf19b9ee4d630f66
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-05-26 19:56:32 +0300

    create GradientLinearProgressIndicator

---
+++ lib/features/sorting/base/widgets/linear_progress_indicator.dart
@@ -0,0 +1,48 @@
+
+import 'package:algorithm_visualizer/core/resources/color_manager.dart';
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class GradientLinearProgressIndicator extends StatelessWidget {
+  final double value;
+
+  const GradientLinearProgressIndicator({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: RSizedBox(
+        height: 3,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: context.getColor(ThemeEnum.glassColor),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+
+            FractionallySizedBox(
+              widthFactor: value,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: AlignmentDirectional.centerStart,
+                    end: AlignmentDirectional.centerEnd,
+                    colors: [
+                      ColorManager.mainDarkColor,
+                      ColorManager.main2DarkColor,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
