b0e7114ad1c9134560304531757cdfd0e70a9258
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-05-26 19:56:05 +0300

    create AlgorithmComplexity

---
+++ lib/core/helpers/o_notation.dart
@@ -0,0 +1,39 @@
+class AlgorithmComplexity {
+  final String name;
+  final ONotationComplexity bestTimeComplexity;
+  final ONotationComplexity averageTimeComplexity;
+  final ONotationComplexity worstTimeComplexity;
+  final ONotationComplexity spaceComplexity;
+  AlgorithmComplexity({
+    required this.name,
+    required this.spaceComplexity,
+    required this.averageTimeComplexity,
+    required this.worstTimeComplexity,
+    required this.bestTimeComplexity,
+  });
+}
+
+enum ONotationComplexity { constant, logN, nLogN, nk, nPlusK, k, n, n2 }
+
+extension ONotationComplexityExt on ONotationComplexity {
+  String get getText {
+    switch (this) {
+      case ONotationComplexity.constant:
+        return "O(1)";
+      case ONotationComplexity.logN:
+        return "O(log n)";
+      case ONotationComplexity.nLogN:
+        return "O(n log n)";
+      case ONotationComplexity.n:
+        return "O(n)";
+      case ONotationComplexity.k:
+        return "O(k)";
+      case ONotationComplexity.nk:
+        return "O(nk)";
+      case ONotationComplexity.nPlusK:
+        return "O(n + k)";
+      case ONotationComplexity.n2:
+        return "O(n²)";
+    }
+  }
+}
