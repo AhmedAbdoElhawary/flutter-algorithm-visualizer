d0dbdc134d8f4fcc6d53ff10eb94fd57eacbb8df
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-05-26 19:51:04 +0300

    add complexity details

--- lib/features/sorting/bubble/view_model/bubble_sort_notifier.dart
+++ lib/features/sorting/bubble/view_model/bubble_sort_notifier.dart
@@ -1,3 +1,5 @@
+import 'package:algorithm_visualizer/core/helpers/o_notation.dart';
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
 import 'package:collection/collection.dart';
 
@@ -31,4 +33,20 @@ class BubbleSortNotifier extends SortingNotifier {
 
     return SortingResult(sortedValues: arr, steps: steps);
   }
+
+  static final algorithmComplexity = AlgorithmComplexity(
+    name: StringsManager.bubbleSort,
+    bestTimeComplexity: ONotationComplexity.n,
+    averageTimeComplexity: ONotationComplexity.n2,
+    worstTimeComplexity: ONotationComplexity.n2,
+    spaceComplexity: ONotationComplexity.constant,
+  );
+
+  @override
+  AlgorithmComplexity get algoComplexity => algorithmComplexity;
+
+  @override
+  String get description => StringsManager.bubbleSortDescription;
+
+
 }
