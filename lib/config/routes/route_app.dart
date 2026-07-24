417c04fcbc245dd68ebbd9e83e7b923b1efb2ca1
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-07-24 16:43:48 +0300

    add some routes

--- lib/config/routes/route_app.dart
+++ lib/config/routes/route_app.dart
@@ -2,6 +2,7 @@ import 'package:algorithm_visualizer/core/helpers/app_bar/app_bar.dart';
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/features/base/view/base_page.dart';
+import 'package:algorithm_visualizer/features/searching/pathfinding/visualizer_screen.dart';
 import 'package:algorithm_visualizer/features/sorting/bubble/view/bubble_sort_page.dart';
 import 'package:algorithm_visualizer/features/sorting/bucket/view/bucket_sort_page.dart';
 import 'package:algorithm_visualizer/features/sorting/comparison/view/comparison_sort_page.dart';
@@ -15,12 +16,25 @@ import 'package:algorithm_visualizer/features/sorting/selection/view/selection_s
 import 'package:algorithm_visualizer/features/sorting/shell/view/shell_sort_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 part 'unknown_page.dart';
 
 class Routes {
   static const RouteConfig base = RouteConfig(
     name: 'base',
     path: '/',
+  );
+  static const RouteConfig bfsSearching = RouteConfig(
+    name: 'bfsSearching',
+    path: '/bfsSearching',
+  );
+  static const RouteConfig dfsSearching = RouteConfig(
+    name: 'dfsSearching',
+    path: '/dfsSearching',
+  );
+  static const RouteConfig aStarSearching = RouteConfig(
+    name: 'aStarSearching',
+    path: '/aStarSearching',
   );
 
   static const RouteConfig bubbleSort = RouteConfig(
@@ -98,6 +112,11 @@ class AppRoutes {
             path: Routes.base.path,
             name: Routes.base.name,
             builder: (context, state) => const BasePage(),
+          ),
+          GoRoute(
+            path: Routes.bfsSearching.path,
+            name: Routes.bfsSearching.name,
+            builder: (context, state) => const BFSSearchingPage(),
           ),
           GoRoute(
             path: Routes.bubbleSort.path,
