e43e4f97d15976998d6650bcfe4802afeeb1770d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-07-24 16:55:52 +0300

    create BFSSearchingPage

---
+++ lib/features/searching/pathfinding/bfs_searching.dart
@@ -0,0 +1,14 @@
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
+import 'package:algorithm_visualizer/features/base/view_model/base_page_view_model.dart';
+import 'package:algorithm_visualizer/features/searching/pathfinding/visualizer_page.dart';
+import 'package:flutter/material.dart';
+
+class BFSSearchingPage extends StatelessWidget {
+  const BFSSearchingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final instance = BasePageViewModel.searchingCards[StringsManager.bFS]!.instance;
+    return VisualizerScreen(instance: instance, title: StringsManager.bFS);
+  }
+}
