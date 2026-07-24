2fa7779128d48a01ea4fc1d156786ac080f81371
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-07-24 16:56:32 +0300

    add some routes

--- lib/config/routes/route_app.dart
+++ lib/config/routes/route_app.dart
@@ -2,7 +2,9 @@ import 'package:algorithm_visualizer/core/helpers/app_bar/app_bar.dart';
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/features/base/view/base_page.dart';
-import 'package:algorithm_visualizer/features/searching/pathfinding/visualizer_screen.dart';
+import 'package:algorithm_visualizer/features/searching/pathfinding/a_star_searching.dart';
+import 'package:algorithm_visualizer/features/searching/pathfinding/bfs_searching.dart';
+import 'package:algorithm_visualizer/features/searching/pathfinding/dfs_searching.dart';
 import 'package:algorithm_visualizer/features/sorting/bubble/view/bubble_sort_page.dart';
 import 'package:algorithm_visualizer/features/sorting/bucket/view/bucket_sort_page.dart';
 import 'package:algorithm_visualizer/features/sorting/comparison/view/comparison_sort_page.dart';
@@ -117,6 +119,16 @@ class AppRoutes {
             path: Routes.bfsSearching.path,
             name: Routes.bfsSearching.name,
             builder: (context, state) => const BFSSearchingPage(),
+          ),
+          GoRoute(
+            path: Routes.dfsSearching.path,
+            name: Routes.dfsSearching.name,
+            builder: (context, state) => const DFSSearchingPage(),
+          ),
+          GoRoute(
+            path: Routes.aStarSearching.path,
+            name: Routes.aStarSearching.name,
+            builder: (context, state) => const AStarSearchingPage(),
           ),
           GoRoute(
             path: Routes.bubbleSort.path,
