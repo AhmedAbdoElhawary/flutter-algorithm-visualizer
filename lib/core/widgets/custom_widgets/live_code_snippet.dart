110fae047359492854eb5afaa8e50dc0a2f99c65
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-02 21:29:47 +0300

    add info popup and read-only mode to live code snippet

--- lib/core/widgets/custom_widgets/live_code_snippet.dart
+++ lib/core/widgets/custom_widgets/live_code_snippet.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:algorithm_visualizer/core/custom_packages/custom_code_editor/code_editor.dart';
 import 'package:algorithm_visualizer/core/resources/color_manager.dart';
+import 'package:algorithm_visualizer/features/searching/base/widgets/pf_note.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -25,9 +28,93 @@ class _CodeEditor extends StatefulWidget {
   State<_CodeEditor> createState() => _CodeEditorState();
 }
 
-class _CodeEditorState extends State<_CodeEditor> {
+class _CodeEditorState extends State<_CodeEditor> with SingleTickerProviderStateMixin {
+  final LayerLink _layerLink = LayerLink();
   late final CodeController controller;
   final border = Border.all(color: const Color.fromRGBO(36, 40, 46, 1), width: 1.r);
+
+  OverlayEntry? _overlay;
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 220),
+  );
+
+  void _togglePopup() {
+    if (_overlay != null) {
+      _removePopup();
+    } else {
+      _showPopup();
+    }
+  }
+
+  void _showPopup() {
+    _overlay = OverlayEntry(
+      builder: (context) {
+        final width = MediaQuery.sizeOf(context).width * 0.89;
+        return Stack(
+          children: [
+            // Blur the entire screen
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: _removePopup,
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 4,
+                    sigmaY:4,
+                  ),
+                  child: Container(
+                    color: ColorManager.primaryDarkColor
+                        .withValues(alpha: 0.2),
+                  ),
+                ),
+              ),
+            ),
+
+            CompositedTransformFollower(
+              link: _layerLink,
+              showWhenUnlinked: false,
+              offset: Offset(-width, -120.r),
+              child: FadeTransition(
+                opacity: CurvedAnimation(
+                  parent: _controller,
+                  curve: Curves.easeOut,
+                ),
+                child: ScaleTransition(
+                  scale: Tween<double>(
+                    begin: 0.9,
+                    end: 1,
+                  ).animate(
+                    CurvedAnimation(
+                      parent: _controller,
+                      curve: Curves.easeOutBack,
+                    ),
+                  ),
+                  alignment: Alignment.topRight,
+                  child: const Material(
+                    color: Colors.transparent,
+                    child: HowItWorks(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
+    Overlay.of(context).insert(_overlay!);
+    _controller.forward(from: 0);
+  }
+
+  void _removePopup() async {
+    if (_overlay == null) return;
+
+    await _controller.reverse();
+
+    _overlay?.remove();
+    _overlay = null;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -107,6 +194,8 @@ class _CodeEditorState extends State<_CodeEditor> {
   @override
   void dispose() {
     controller.dispose();
+    _removePopup();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -135,10 +224,23 @@ class _CodeEditorState extends State<_CodeEditor> {
                 CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(182, 142, 43, 1)),
                 RSizedBox(width: 5),
                 CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(46, 156, 117, 1)),
+                Spacer(),
+                CompositedTransformTarget(
+                  link: _layerLink,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: _togglePopup,
+                    child: Icon(
+                      Icons.lightbulb,
+                      size: 16.r,
+                      color: const Color.fromRGBO(213, 209, 17, 1),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-          Flexible(child: CodeEditor(controller: controller)),
+          Flexible(child: CodeEditor(controller: controller, readOnly: true)),
         ],
       ),
     );
