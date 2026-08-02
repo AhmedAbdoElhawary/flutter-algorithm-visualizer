7508e00a2a074d675b6b640303f118602077a16c
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-02 20:37:13 +0300

    rename visualizer_page.dart to searching_page.dart

--- lib/features/searching/dfs/view/dfs_searching.dart
+++ lib/features/searching/dfs/view/dfs_searching.dart
@@ -1,6 +1,6 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_page_view_model.dart';
-import 'package:algorithm_visualizer/features/searching/base/view/visualizer_page.dart';
+import 'package:algorithm_visualizer/features/searching/base/view/searching_page.dart';
 import 'package:flutter/material.dart';
 
 class DFSSearchingPage extends StatelessWidget {
