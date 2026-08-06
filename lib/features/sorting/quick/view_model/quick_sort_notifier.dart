7c197d25d326b0e491fd71e250a9fee73649b71b
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-06 20:10:28 +0300

    reformat code and whitespace

--- lib/features/sorting/quick/view_model/quick_sort_notifier.dart
+++ lib/features/sorting/quick/view_model/quick_sort_notifier.dart
@@ -63,45 +63,44 @@ class QuickSortNotifier extends SortingNotifier {
   String get algorithmDescription => StringsManager.quickSortDescription;
   @override
   List<String> get codeSnippet => const [
-    'void main() {', // 0
-    '  List<int> arr = [64, 34, 25, 12, 22, 11, 90];', // 1
-    '  quickSort(arr, 0, arr.length - 1);', // 2
-    '}', // 3
-    'void quickSort(List<int> arr, int low, int high) {', // 4
-    '  if (low < high) {', // 5
-    '    int pi = partition(arr, low, high);', // 6
-    '    quickSort(arr, low, pi - 1);', // 7
-    '    quickSort(arr, pi + 1, high);', // 8
-    '  }', // 9
-    '}', // 10
-    'int partition(List<int> arr, int low, int high) {', // 11
-    '  int pivot = arr[high];', // 12
-    '  int i = low - 1;', // 13
-    '  for (int j = low; j < high; j++) {', // 14
-    '    if (arr[j] <= pivot) {', // 15
-    '      i++;', // 16
-    '      if (i != j) {', // 17
-    '        int temp = arr[i];', // 18
-    '        arr[i] = arr[j];', // 19
-    '        arr[j] = temp;', // 20
-    '      }', // 21
-    '    }', // 22
-    '  }', // 23
-    '  if (i + 1 != high) {', // 24
-    '    int temp = arr[i + 1];', // 25
-    '    arr[i + 1] = arr[high];', // 26
-    '    arr[high] = temp;', // 27
-    '  }', // 28
-    '  return i + 1;', // 29
-    '}', // 30
-  ];
+        'void main() {', // 0
+        '  List<int> arr = [64, 34, 25, 12, 22, 11, 90];', // 1
+        '  quickSort(arr, 0, arr.length - 1);', // 2
+        '}', // 3
+        'void quickSort(List<int> arr, int low, int high) {', // 4
+        '  if (low < high) {', // 5
+        '    int pi = partition(arr, low, high);', // 6
+        '    quickSort(arr, low, pi - 1);', // 7
+        '    quickSort(arr, pi + 1, high);', // 8
+        '  }', // 9
+        '}', // 10
+        'int partition(List<int> arr, int low, int high) {', // 11
+        '  int pivot = arr[high];', // 12
+        '  int i = low - 1;', // 13
+        '  for (int j = low; j < high; j++) {', // 14
+        '    if (arr[j] <= pivot) {', // 15
+        '      i++;', // 16
+        '      if (i != j) {', // 17
+        '        int temp = arr[i];', // 18
+        '        arr[i] = arr[j];', // 19
+        '        arr[j] = temp;', // 20
+        '      }', // 21
+        '    }', // 22
+        '  }', // 23
+        '  if (i + 1 != high) {', // 24
+        '    int temp = arr[i + 1];', // 25
+        '    arr[i + 1] = arr[high];', // 26
+        '    arr[high] = temp;', // 27
+        '  }', // 28
+        '  return i + 1;', // 29
+        '}', // 30
+      ];
 
   @override
   int codeLineForStep(SortingStep step) => switch (step.action) {
-    SortingStatus.compared                              => 15, // arr[j] <= pivot
-    SortingStatus.swapping                              => 19, // arr[i] = arr[j]
- SortingStatus.none     => 14, // advance j
-    _ => -1,
-  };
-
+        SortingStatus.compared => 15, // arr[j] <= pivot
+        SortingStatus.swapping => 19, // arr[i] = arr[j]
+        SortingStatus.none => 14, // advance j
+        _ => -1,
+      };
 }
