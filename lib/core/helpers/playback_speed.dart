41ac2ac22dabe371bbc2a1c2e3706e72a5cc09a9
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-07 10:17:43 +0300

    separate playback speed durations for searching and sorting

--- lib/core/helpers/playback_speed.dart
+++ lib/core/helpers/playback_speed.dart
@@ -1,12 +1,19 @@
 enum PlaybackSpeed { slow, normal, fast3, fast5, fast10 }
 
 extension PlaybackSpeedX on PlaybackSpeed {
-  Duration get stepDuration => switch (this) {
+  Duration get stepSearchingDuration => switch (this) {
         PlaybackSpeed.slow => const Duration(milliseconds: 300),
         PlaybackSpeed.normal => const Duration(milliseconds: 150),
         PlaybackSpeed.fast3 => const Duration(milliseconds: 55),
         PlaybackSpeed.fast5 => const Duration(milliseconds: 16),
         PlaybackSpeed.fast10 => const Duration(milliseconds: 10),
+      };
+  Duration get stepSortingDuration => switch (this) {
+        PlaybackSpeed.slow => const Duration(milliseconds: 900),
+        PlaybackSpeed.normal => const Duration(milliseconds: 300),
+        PlaybackSpeed.fast3 => const Duration(milliseconds: 150),
+        PlaybackSpeed.fast5 => const Duration(milliseconds: 100),
+        PlaybackSpeed.fast10 => const Duration(milliseconds: 50),
       };
 
   int get level => switch (this) {
