48e0ea07010249425283daa0de2d563792e1d3a3
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-09 21:51:59 +0300

    update bubble sort page to use HomePageViewModel for sorting cards

--- lib/features/sorting/bubble/view/bubble_sort_page.dart
+++ lib/features/sorting/bubble/view/bubble_sort_page.dart
@@ -1,5 +1,5 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
-import 'package:algorithm_visualizer/features/base/view_model/base_page_view_model.dart';
+import 'package:algorithm_visualizer/features/home/view_model/home_page_view_model.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
 import 'package:algorithm_visualizer/features/sorting/bubble/view_model/bubble_sort_notifier.dart';
@@ -16,7 +16,7 @@ class BubbleSortPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SortingPage(
-        instance: BasePageViewModel.sortingCards[StringsManager.bubbleSort]?.instance ?? _notifierProvider,
+        instance: HomePageViewModel.sortingCards[StringsManager.bubbleSort]?.instance ?? _notifierProvider,
         title: StringsManager.bubbleSort);
   }
 }
