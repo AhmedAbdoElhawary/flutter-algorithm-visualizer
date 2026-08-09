14cac278c2e1c202e788039988f192ba5b4a1136
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-09 21:54:14 +0300

    add visualize page widget

---
+++ lib/features/visualize/view/visualize_page.dart
@@ -0,0 +1,11 @@
+import 'package:algorithm_visualizer/features/sorting/bubble/view/bubble_sort_page.dart';
+import 'package:flutter/material.dart';
+
+class VisualizePage extends StatelessWidget {
+  const VisualizePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BubbleSortPage();
+  }
+}
