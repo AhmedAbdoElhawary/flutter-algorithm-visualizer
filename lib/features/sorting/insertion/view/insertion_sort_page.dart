177479fef3bb3e2cfa21179084189fb3725b494d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-09 21:52:56 +0300

    update sorting pages to use HomePageViewModel and relocate movable button

--- lib/features/sorting/insertion/view/insertion_sort_page.dart
+++ lib/features/sorting/insertion/view/insertion_sort_page.dart
@@ -1,5 +1,5 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
-import 'package:algorithm_visualizer/features/base/view_model/base_page_view_model.dart';
+import 'package:algorithm_visualizer/features/home/view_model/home_page_view_model.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
 import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
 import 'package:algorithm_visualizer/features/sorting/insertion/view_model/insertion_sort_notifier.dart';
@@ -16,7 +16,7 @@ class InsertionSortPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SortingPage(
-        instance: BasePageViewModel.sortingCards[StringsManager.insertionSort]?.instance ?? _notifierProvider,
+        instance: HomePageViewModel.sortingCards[StringsManager.insertionSort]?.instance ?? _notifierProvider,
         title: StringsManager.insertionSort);
   }
 }
