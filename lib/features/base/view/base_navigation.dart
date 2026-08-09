680629f9043edb342da55a3312ba44f588cefeeb
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-09 21:51:51 +0300

    add main navigation shell with cupertino tab bar

---
+++ lib/features/base/view/base_navigation.dart
@@ -0,0 +1,76 @@
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+class MainNavigationShell extends StatefulWidget {
+  const MainNavigationShell({
+    super.key,
+    required this.navigationShell,
+  });
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  State<MainNavigationShell> createState() => _MainNavigationShellState();
+}
+
+class _MainNavigationShellState extends State<MainNavigationShell> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          widget.navigationShell,
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: CupertinoTabBar(
+              activeColor: context.getColor(ThemeEnum.darkBlueColor),
+              inactiveColor: context.getColor(ThemeEnum.codeEditorNumberColor),
+              iconSize: 22.r,
+              backgroundColor: context.getColor(ThemeEnum.primaryColor),
+              border: Border(top: BorderSide(width: 1, color: context.getColor(ThemeEnum.shadowColor))),
+              currentIndex: widget.navigationShell.currentIndex,
+              onTap: (index) {
+                widget.navigationShell.goBranch(
+                  index,
+                  initialLocation: index == widget.navigationShell.currentIndex,
+                );
+              },
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  activeIcon: Icon(Icons.home_rounded),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart_rounded),
+                  activeIcon: Icon(Icons.bar_chart_rounded),
+                  label: 'Visual',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.code_rounded),
+                  activeIcon: Icon(Icons.code_rounded),
+                  label: 'Code',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.emoji_events_outlined),
+                  activeIcon: Icon(Icons.emoji_events_rounded),
+                  label: 'Practice',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline_rounded),
+                  activeIcon: Icon(Icons.person_rounded),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
