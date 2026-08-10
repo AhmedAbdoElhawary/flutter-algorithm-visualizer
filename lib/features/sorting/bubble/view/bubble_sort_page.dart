ab16bbc437ef633b6dcfc2fd23c4d342f3c228b6
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:46:11 +0300

    refactor searching and sorting pages to use SearchingView and SortingView components

--- lib/features/sorting/bubble/view/bubble_sort_page.dart
+++ lib/features/sorting/bubble/view/bubble_sort_page.dart
@@ -1,22 +1,13 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/bubble/view_model/bubble_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => BubbleSortNotifier(),
-);
 
 class BubbleSortPage extends StatelessWidget {
   const BubbleSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.bubbleSort]?.instance ?? _notifierProvider,
-        title: StringsManager.bubbleSort);
+    return SortingView(
+        card: SortingAlgoCards.bubble);
   }
 }
