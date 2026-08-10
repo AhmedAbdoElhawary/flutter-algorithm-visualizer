441ed676c14c5832a791bc439d79cd15364c9a58
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 21:34:45 +0300

    add challenges notifier for practice state management

---
+++ lib/features/practice/view_model/challenges_notifier.dart
@@ -0,0 +1,24 @@
+import 'package:algorithm_visualizer/features/practice/helper/problem.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'challenges_state.dart';
+
+class ChallengesNotifier extends Notifier<ChallengesState> {
+  static const filters = ProblemDifficulty.values;
+
+  @override
+  ChallengesState build() => const ChallengesState();
+
+  void setFilter(ProblemDifficulty filter) => state = state.copyWith(filter: filter);
+
+  void setSearch(String query) => state = state.copyWith(search: query);
+
+  void clearSearch() => state = state.copyWith(search: '');
+
+  void toggleExpanded(int problemId) {
+    state = state.copyWith(expandedIds: problemId);
+  }
+}
+
+final challengesProvider =
+NotifierProvider<ChallengesNotifier, ChallengesState>(ChallengesNotifier.new);
