e95ab2f4065730ee16e24f19d25bf3a3756fd947
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:47:22 +0300

    refactor searching and sorting pages into view components and update card selection logic

--- lib/features/sorting/selection/view/selection_sort_page.dart
+++ lib/features/sorting/selection/view/selection_sort_page.dart
@@ -1,22 +1,12 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/selection/view_model/selection_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => SelectionSortNotifier(),
-);
 
 class SelectionSortPage extends StatelessWidget {
   const SelectionSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.selectionSort]?.instance ?? _notifierProvider,
-        title: StringsManager.selectionSort);
+    return SortingView(card: SortingAlgoCards.selection);
   }
 }
