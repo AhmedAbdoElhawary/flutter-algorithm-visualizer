8cdf386312c4352a2feb4bf678d6467cad5c677e
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:46:54 +0300

    refactor algorithm card access logic and update insertion sort view

--- lib/features/sorting/insertion/view/insertion_sort_page.dart
+++ lib/features/sorting/insertion/view/insertion_sort_page.dart
@@ -1,22 +1,12 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/insertion/view_model/insertion_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => InsertionSortNotifier(),
-);
 
 class InsertionSortPage extends StatelessWidget {
   const InsertionSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.insertionSort]?.instance ?? _notifierProvider,
-        title: StringsManager.insertionSort);
+    return SortingView(card: SortingAlgoCards.insertion);
   }
 }
