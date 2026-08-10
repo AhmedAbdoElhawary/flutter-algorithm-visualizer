499a24f7814a308b198e6121d8337b585e04c789
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 06:37:10 +0300

    add base interfaces and view model for algorithm visualization registry

---
+++ lib/features/base/view_model/algorithm_control_interface.dart
@@ -0,0 +1,14 @@
+import 'package:algorithm_visualizer/core/helpers/playback_speed.dart';
+import 'package:flutter/material.dart';
+
+abstract class AlgorithmControlInterface {
+  void reset();
+  void stepBackward();
+  void stepForward();
+  Future<void> togglePlay(BuildContext context);
+  void changeSpeed(PlaybackSpeed speed);
+  bool get backwardValidation;
+  bool get forwardValidation;
+  bool get isPlaying;
+  PlaybackSpeed get getSpeed;
+}
