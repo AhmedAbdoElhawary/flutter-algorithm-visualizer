059fad2f9afbe2fc05eabe5e879fec1f0375bf45
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:47:11 +0300

    refactor quick and radix sort pages to use SortingView

--- lib/features/sorting/radix/view/radix_sort_page.dart
+++ lib/features/sorting/radix/view/radix_sort_page.dart
@@ -1,22 +1,12 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/radix/view_model/radix_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => RadixSortNotifier(),
-);
 
 class RadixSortPage extends StatelessWidget {
   const RadixSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.radixSort]?.instance ?? _notifierProvider,
-        title: StringsManager.radixSort);
+    return SortingView(card: SortingAlgoCards.radix);
   }
 }
