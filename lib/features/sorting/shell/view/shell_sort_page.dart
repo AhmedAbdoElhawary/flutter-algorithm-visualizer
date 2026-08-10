5c88713b153df6348cfd03151f2f12d21e650a2c
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:47:30 +0300

    refactor shell sort page to use SortingView

--- lib/features/sorting/shell/view/shell_sort_page.dart
+++ lib/features/sorting/shell/view/shell_sort_page.dart
@@ -1,22 +1,14 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/shell/view_model/shell_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => ShellSortNotifier(),
-);
 
 class ShellSortPage extends StatelessWidget {
   const ShellSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.shellSort]?.instance ?? _notifierProvider,
-        title: StringsManager.shellSort);
+    return SortingView(
+        card: SortingAlgoCards.shell);
   }
 }
