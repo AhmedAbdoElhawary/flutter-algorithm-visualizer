b291cb4a8976f3b69560916ca836a7374e3ee844
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:46:32 +0300

    refactor counting sort page to use SortingView and simplified card configuration

--- lib/features/sorting/counting/view/counting_sort_page.dart
+++ lib/features/sorting/counting/view/counting_sort_page.dart
@@ -1,22 +1,12 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/counting/view_model/counting_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => CountingSortNotifier(),
-);
 
 class CountingSortPage extends StatelessWidget {
   const CountingSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.countingSort]?.instance ?? _notifierProvider,
-        title: StringsManager.countingSort);
+    return SortingView(card: SortingAlgoCards.counting);
   }
 }
