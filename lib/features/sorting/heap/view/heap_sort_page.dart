bbdac275b5e4f41ee886732a75977cfabb045336
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 08:46:45 +0300

    refactor searching and sorting pages to use SearchingView and SortingView

--- lib/features/sorting/heap/view/heap_sort_page.dart
+++ lib/features/sorting/heap/view/heap_sort_page.dart
@@ -1,22 +1,13 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/base/view_model/base_view_model.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view/sorting_page.dart';
-import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
-import 'package:algorithm_visualizer/features/sorting/heap/view_model/heap_sort_notifier.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view/sorting_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final _notifierProvider = StateNotifierProvider<SortingNotifier, SortingNotifierState>(
-  (ref) => HeapSortNotifier(),
-);
 
 class HeapSortPage extends StatelessWidget {
   const HeapSortPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SortingPage(
-        instance: BaseViewModel.baseCategory.sortingCards[StringsManager.heapSort]?.instance ?? _notifierProvider,
-        title: StringsManager.heapSort);
+    return SortingView(
+        card: SortingAlgoCards.heap);
   }
 }
