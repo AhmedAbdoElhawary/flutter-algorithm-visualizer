2ce72999dbecccec76f9fe484a14fd159bfe3630
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-10 06:37:10 +0300

    add base interfaces and view model for algorithm visualization registry

---
+++ lib/features/base/view_model/algorithm_description_interface.dart
@@ -0,0 +1,16 @@
+import 'package:algorithm_visualizer/core/helpers/o_notation.dart';
+import 'package:algorithm_visualizer/features/sorting/base/view_model/sorting_notifier.dart';
+
+abstract class AlgorithmDescriptionNotifier {
+  AlgorithmComplexity get algoComplexity;
+
+  String get algorithmDescription;
+
+  List<String> get codeSnippet;
+
+  int codeLineForStep(SortingStep step);
+}
+
+extension AlgorithmNotifierExt on AlgorithmDescriptionNotifier {
+  String get code => codeSnippet.join('\n');
+}
