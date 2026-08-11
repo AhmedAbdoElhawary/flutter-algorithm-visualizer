9674fdcdd9dccee493e12c7903b2d8c93bfaa4fc
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-11 16:07:57 +0300

    add line highlighting and execution status to code editor and remove info popup

--- lib/core/widgets/custom_widgets/code_editor.dart
+++ lib/core/widgets/custom_widgets/code_editor.dart
@@ -1,130 +1,32 @@
-import 'dart:ui';
 import 'package:algorithm_visualizer/core/custom_packages/custom_code_editor/code_editor.dart';
 import 'package:algorithm_visualizer/core/resources/color_manager.dart';
-import 'package:algorithm_visualizer/core/widgets/custom_widgets/how_it_works.dart';
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CodeEditorBlock extends StatefulWidget {
-  const CodeEditorBlock(this.code, {super.key});
+  const CodeEditorBlock({
+    required this.code,
+    this.highlightLineNumber = -1,
+    this.executing = false,
+    super.key,
+  });
   final String code;
+  final int highlightLineNumber;
+  final bool executing;
   @override
   State<CodeEditorBlock> createState() => _CodeEditorBlockState();
 }
 
 class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProviderStateMixin {
-  final LayerLink _layerLink = LayerLink();
-  late final CodeController controller;
   final border = Border.all(color: const Color.fromRGBO(36, 40, 46, 1), width: 1.r);
-
-  OverlayEntry? _overlay;
-
-  late final AnimationController _controller = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 320),
-  );
-
-  late final Animation<double> _scale;
-  late final Animation<double> _opacity;
-  late final Animation<Offset> _offset;
-
-  void _togglePopup() {
-    if (_overlay != null) {
-      _removePopup();
-    } else {
-      _showPopup();
-    }
-  }
-
-  void _showPopup() {
-    _overlay = OverlayEntry(
-      builder: (context) {
-        final width = MediaQuery.sizeOf(context).width * 0.89;
-        return Stack(
-          children: [
-            Positioned.fill(
-              child: GestureDetector(
-                onTap: _removePopup,
-                child: AnimatedBuilder(
-                  animation: _controller,
-                  builder: (_, child) {
-                    final value = Curves.easeOut.transform(_controller.value);
-
-                    return BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: value * 12,
-                        sigmaY: value * 12,
-                      ),
-                      child: Container(
-                        color: Colors.black.withValues(alpha: value * 0.18),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            CompositedTransformFollower(
-              link: _layerLink,
-              showWhenUnlinked: false,
-              offset: Offset(-width, -120.r),
-              child: FadeTransition(
-                opacity: _opacity,
-                child: SlideTransition(
-                  position: _offset,
-                  child: ScaleTransition(
-                    scale: _scale,
-                    alignment: Alignment.topRight,
-                    child: const Material(
-                      color: Colors.transparent,
-                      child: HowItWorks(),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-
-    Overlay.of(context).insert(_overlay!);
-    _controller.forward(from: 0);
-  }
-
-  void _removePopup() async {
-    if (_overlay == null) return;
-
-    await _controller.reverse();
-
-    _overlay?.remove();
-    _overlay = null;
-  }
+  late final CodeController controller;
 
   @override
   void initState() {
     super.initState();
-
-    final curve = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeOutCubic,
-      reverseCurve: Curves.easeInCubic,
-    );
-
-    _scale = Tween(
-      begin: 0.15,
-      end: 1.0,
-    ).animate(curve);
-
-    _opacity = Tween(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(curve);
-
-    _offset = Tween(
-      begin: const Offset(0.08, -0.08),
-      end: Offset.zero,
-    ).animate(curve);
-
     controller = CodeController(
       text: widget.code,
       tokenizer: const DartTokenizer(),
@@ -153,36 +55,20 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
           height: 1.5,
         ),
         tokenColors: const <TokenType, Color>{
-          // function, if, else, while, let, const, return...
           TokenType.keyword: Color(0xFFC792EA),
-
-          // "hello"
           TokenType.string: Color(0xFF98C379),
-
-          // 0, 1, 23...
           TokenType.number: Color(0xFFF78C6C),
-
-          // // comments
           TokenType.comment: Color(0xFF5C6A8A),
-
-          // variable names
           TokenType.identifier: Color(0xFFD6DEEB),
-
-          // =, +, -, <=, === ...
           TokenType.operator: Color(0xFF89DDFF),
-
-          // { } ( ) [ ] ; , .
           TokenType.punctuation: Color(0xFFD6DEEB),
-
-          // Built-in objects/functions
-          // Math, console, Array, String...
           TokenType.builtin: Color(0xFF82AAFF),
-
-          // Plain text
           TokenType.plain: Color(0xFFD6DEEB),
         },
       ),
     );
+
+    _highlightLine();
 
     // final result = controller.execute();
     // if (result.success) {
@@ -193,10 +79,22 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
   }
 
   @override
+  void didUpdateWidget(covariant CodeEditorBlock oldWidget) {
+    if (oldWidget.highlightLineNumber != widget.highlightLineNumber) _highlightLine();
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _highlightLine() {
+    if (widget.highlightLineNumber < 1) return;
+    controller.clearHighlights();
+
+    controller.highlightLine(widget.highlightLineNumber, ColorManager.highlightCodeLineColor);
+  }
+
+  @override
   void dispose() {
     controller.dispose();
-    _removePopup();
-    _controller.dispose();
     super.dispose();
   }
 
@@ -205,13 +103,12 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-        // color: ColorManager.codeEditorBackground
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            padding: REdgeInsetsDirectional.all(12),
+            padding: REdgeInsetsDirectional.only(start: 12, end: 12),
             decoration: BoxDecoration(
               color: Color.fromRGBO(22, 27, 34, 1),
               borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
@@ -219,24 +116,17 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
             ),
             child: Row(
               children: [
-                CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(181, 88, 89, 1)),
+                Padding(
+                  padding: REdgeInsetsDirectional.only(top: 12, bottom: 12),
+                  child: CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(181, 88, 89, 1)),
+                ),
                 RSizedBox(width: 5),
                 CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(182, 142, 43, 1)),
                 RSizedBox(width: 5),
                 CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(46, 156, 117, 1)),
                 Spacer(),
-                CompositedTransformTarget(
-                  link: _layerLink,
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(20),
-                    onTap: _togglePopup,
-                    child: Icon(
-                      Icons.lightbulb,
-                      size: 16.r,
-                      color: const Color.fromRGBO(213, 209, 17, 1),
-                    ),
-                  ),
-                ),
+                if (widget.executing)
+                  MediumText(StringsManager.executing, color: ThemeEnum.greenColor, fontSize: 10)
               ],
             ),
           ),
