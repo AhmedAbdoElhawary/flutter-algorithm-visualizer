eb8cd985a4416185859be068d16555ac6fce33aa
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-11 22:14:47 +0300

    update problem style to use ThemeManager for colors

--- lib/features/practice/helper/problem_style.dart
+++ lib/features/practice/helper/problem_style.dart
@@ -1,3 +1,4 @@
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/features/practice/helper/problem.dart';
 import 'package:algorithm_visualizer/lib-temp/theme/app_theme.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +9,11 @@ class ProblemStyle {
   static Color difficultyColor(BuildContext context, ProblemDifficulty difficulty) {
     switch (difficulty) {
       case ProblemDifficulty.easy:
-        return context.accentGreen;
+        return context.getColor(ThemeEnum.accentGreen);
       case ProblemDifficulty.medium:
-        return context.accentYellow;
+        return context.getColor(ThemeEnum.accentYellow);
       case ProblemDifficulty.hard:
-        return context.accentRed;
+        return context.getColor(ThemeEnum.accentRed);
       default:
         return context.textVMuted;
     }
@@ -21,9 +22,9 @@ class ProblemStyle {
   static Color statusColor(BuildContext context, ProblemStatus status) {
     switch (status) {
       case ProblemStatus.solved:
-        return context.accentGreen;
+        return context.getColor(ThemeEnum.accentGreen);
       case ProblemStatus.attempted:
-        return context.accentYellow;
+        return context.getColor(ThemeEnum.accentYellow);
       default:
         return context.textVMuted;
     }
