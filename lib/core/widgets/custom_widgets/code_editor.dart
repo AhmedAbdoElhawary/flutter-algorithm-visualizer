adbb6a24c802cc5d9d8e3c8ccfba81317491f1d4
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-11 22:11:20 +0300

    update code editor highlight and executing status colors

--- lib/core/widgets/custom_widgets/code_editor.dart
+++ lib/core/widgets/custom_widgets/code_editor.dart
@@ -96,7 +96,7 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
     if (widget.highlightLineNumber < 1) return;
     controller.clearHighlights();
 
-    controller.highlightLine(widget.highlightLineNumber, ColorManager.highlightCodeLineColor);
+    controller.highlightLine(widget.highlightLineNumber, ColorManager.accentDk);
   }
 
   @override
@@ -132,8 +132,7 @@ class _CodeEditorBlockState extends State<CodeEditorBlock> with SingleTickerProv
                 RSizedBox(width: 5),
                 CircleAvatar(radius: 5.r, backgroundColor: Color.fromRGBO(46, 156, 117, 1)),
                 Spacer(),
-                if (widget.executing)
-                  MediumText(StringsManager.executing, color: ThemeEnum.greenColor, fontSize: 10)
+                if (widget.executing) MediumText(StringsManager.executing, color: ThemeEnum.accentGreen, fontSize: 10)
               ],
             ),
           ),
