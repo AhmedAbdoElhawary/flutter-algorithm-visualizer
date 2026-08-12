bb5c30744d47bdc0fc6be01d145a27d6800a5696
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-12 17:57:50 +0300

    refactor ColorManager to categorize theme colors and remove unused constants

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 
-/// [ColorManager]
-/// colorD means that get dark version from the color for example:
-/// greyD2 means that get version 2 darker from grey.
-/// "less darker D1 < D2 < D4 < D7 < D9 more darker"
-/// ------------------------>
-/// colorL means that get light version from the color for example:
-/// blackL4 means that get version 4 lighter from dark.
-/// "less lighter L1 < L2 < L3 < L5 < L8 more lighter"
-/// ------------------------>
-/// colorOp means that get less opacity version from the main color for example:
-/// blackT1 means that get the same main color "black" but with less opacity.
-/// opacity  for main black:
-/// black => 100%
-/// blackOp90 => 90%
-/// blackOp80 => 80%
-/// blackOp70 => 70%
-/// blackOp60 => 60%
-/// blackOp50 => 50%
-/// blackOp40 => 40%
-/// blackOp30 => 30%
-/// blackOp20 => 20%
-/// blackOp10 => 10%
-
 abstract final class ColorManager {
+  /// -------- unified theme ------------>
+
+  static const Color targetSearchingPoint = Color.fromRGBO(66, 8, 99, 1.0);
+  static const Color wallBlack = Color.fromRGBO(22, 68, 101, 1.0);
+
+  /// -------- dark theme Only------------>
+  static const Color main2DarkColor = Color.fromRGBO(192, 132, 252, 1);
+  static const Color columnSortColor = Color.fromRGBO(26, 47, 80, 0.7);
+  static const Color backgroundForSortingColor = Color.fromRGBO(14, 23, 41, 0.3);
+  static const Color white2DarkColor = Color.fromRGBO(148, 163, 184, 1);
+  static const Color textDarkColor = Color.fromRGBO(67, 84, 116, 1);
+  static const Color text2DarkColor = Color.fromRGBO(160, 179, 217, 1.0);
+  static const Color howItWorksColor = Color.fromRGBO(23, 28, 53, 1);
+  static const Color lightPurpleColor = Color.fromRGBO(23, 28, 53, 1.0);
+  static const Color borderPurpleColor = Color.fromRGBO(67, 73, 128, 1.0);
+  static const Color codeEditorBackground = Color.fromRGBO(12, 17, 23, 1);
+  static const Color codeEditorNumberColor = Color.fromRGBO(44, 60, 87, 1);
+
   /// -------- dark theme ------------>
   static const primaryDk = Color.fromRGBO(8, 13, 26, 1);
   static const cardDk = Color.fromRGBO(14, 23, 41, 0.8);
@@ -38,24 +33,7 @@ abstract final class ColorManager {
   static const textSecondDk = Color.fromRGBO(148, 163, 184, 1);
   static const hoverDk = Color.fromRGBO(74, 92, 126, 1);
   static const hoverSecondDk = Color.fromRGBO(42, 58, 85, 1);
-
   static const borderDk = Color.fromRGBO(255, 255, 255, 0.04);
-
-  /// -------- dark theme ------------>
-  static const Color main2DarkColor = Color.fromRGBO(192, 132, 252, 1);
-  static const Color completeColumnSortColor = Color.fromRGBO(25, 42, 69, 1);
-  static const Color columnSortColor = Color.fromRGBO(26, 47, 80, 0.7);
-  static const Color backgroundForSortingColor = Color.fromRGBO(14, 23, 41, 0.3);
-  static const Color white2DarkColor = Color.fromRGBO(148, 163, 184, 1);
-  static const Color textDarkColor = Color.fromRGBO(67, 84, 116, 1);
-  static const Color text2DarkColor = Color.fromRGBO(160, 179, 217, 1.0);
-  static const Color howItWorksColor = Color.fromRGBO(23, 28, 53, 1);
-
-  static const Color lightPurpleColor = Color.fromRGBO(23, 28, 53, 1.0);
-  static const Color borderPurpleColor = Color.fromRGBO(67, 73, 128, 1.0);
-
-  static const Color codeEditorBackground = Color.fromRGBO(12, 17, 23, 1);
-  static const Color codeEditorNumberColor = Color.fromRGBO(44, 60, 87, 1);
 
   /// -------- light theme ------------>
   static const primaryLt = Color.fromRGBO(238, 242, 255, 1);
@@ -71,26 +49,15 @@ abstract final class ColorManager {
   static const textSecondLt = Color.fromRGBO(71, 85, 105, 1);
   static const hoverLt = Color.fromRGBO(100, 116, 139, 1);
   static const hoverSecondLt = Color.fromRGBO(148, 163, 184, 1);
-
   static const borderLt = Color.fromRGBO(0, 0, 0, 0.06);
+
   /// -------------------------------->
 
   static const Color transparent = Colors.transparent;
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
   static const Color whiteD5 = Color.fromRGBO(215, 215, 215, 1);
   static const Color whiteD6 = Color.fromRGBO(205, 205, 205, 1);
-  static const Color whiteD7 = Color.fromRGBO(195, 195, 195, 1);
-
   static const Color grey = Color.fromRGBO(65, 65, 65, 1);
   static const Color grey2 = Color.fromRGBO(75, 75, 75, 1);
-
   static const Color black = Color.fromRGBO(00, 00, 00, 1);
-
-  /// ------------------------------------------>
-
-  static const Color wallBlack = Color.fromRGBO(22, 68, 101, 1.0);
-  static const Color mediumBlue = Color.fromRGBO(111, 149, 230, 1.0);
-  static const Color blackL3Blue = Color.fromRGBO(14, 14, 19, 1.0);
-  static const Color darkPurple = Color.fromRGBO(66, 8, 99, 1.0);
-
 }
