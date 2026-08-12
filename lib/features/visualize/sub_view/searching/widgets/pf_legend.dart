cd72a0260c9e1a66bdb27adc8662de1f71e71119
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-12 17:18:06 +0300

    update pf_legend to use ThemeEnum for label colors and remove unused import

--- lib/features/visualize/sub_view/searching/widgets/pf_legend.dart
+++ lib/features/visualize/sub_view/searching/widgets/pf_legend.dart
@@ -3,7 +3,6 @@ import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/searching/widgets/end_point.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/searching/widgets/pf_grid.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/searching/widgets/start_point.dart';
-import 'package:algorithm_visualizer/lib-temp/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -42,7 +41,7 @@ class PFLegend extends StatelessWidget {
                   decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(2)),
                 ),
               const SizedBox(width: 4),
-              Text(label, style: GoogleFonts.inter(color: context.textMuted, fontSize: 10)),
+              Text(label, style: GoogleFonts.inter(color: context.getColor(ThemeEnum.hover), fontSize: 10)),
             ]);
           }).toList(),
         ),
