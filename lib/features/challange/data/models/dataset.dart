980344d1c7e3eb4a3a814c5f07ce871b8d9753bb
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 14:56:20 +0300

    make Dataset model fields nullable

--- lib/features/challange/data/models/dataset.dart
+++ lib/features/challange/data/models/dataset.dart
@@ -1,4 +1,3 @@
-// lib/models/dataset.dart
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'problem_dto.dart';
 
@@ -8,13 +7,14 @@ part 'dataset.g.dart';
 @freezed
 class Dataset with _$Dataset {
   const factory Dataset({
-    required String name,
-    required String version,
-    required int totalProblems,
-    required String source,
-    required String description,
-    required List<ProblemDTO> problems,
+    required String? name,
+    required String? version,
+    required int? totalProblems,
+    required String? source,
+    required String? description,
+    required List<ProblemDTO>? problems,
   }) = _Dataset;
 
   factory Dataset.fromJson(Map<String, dynamic> json) => _$DatasetFromJson(json);
 }
+
