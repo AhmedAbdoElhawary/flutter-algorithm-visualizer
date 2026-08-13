720bd1757bb5e27c8e24667a0174b207fe9399c2
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 15:03:09 +0300

    remove type field from TestCase model

--- lib/features/challange/data/models/test_case.g.dart
+++ lib/features/challange/data/models/test_case.g.dart
@@ -8,14 +8,12 @@ part of 'test_case.dart';
 
 _$TestCaseImpl _$$TestCaseImplFromJson(Map<String, dynamic> json) =>
     _$TestCaseImpl(
-      type: json['type'] as String,
       input: json['input'] as Map<String, dynamic>,
       expectedOutput: json['expected_output'],
     );
 
 Map<String, dynamic> _$$TestCaseImplToJson(_$TestCaseImpl instance) =>
     <String, dynamic>{
-      'type': instance.type,
       'input': instance.input,
       'expected_output': instance.expectedOutput,
     };
