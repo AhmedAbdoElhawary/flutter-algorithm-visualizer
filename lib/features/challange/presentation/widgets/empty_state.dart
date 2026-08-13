30cdaaeda4cc8ff93302c14d0a39faf574128c06
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 14:56:54 +0300

    move empty_state.dart to challange/presentation and refactor to use StringsManager and adaptive text widgets

--- lib/features/challange/presentation/widgets/empty_state.dart
+++ lib/features/challange/presentation/widgets/empty_state.dart
@@ -1,8 +1,8 @@
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ChallengesEmptyState extends StatelessWidget {
   const ChallengesEmptyState({super.key});
@@ -17,13 +17,9 @@ class ChallengesEmptyState extends StatelessWidget {
           children: [
             RegularText('🔍', fontSize: 40),
             const RSizedBox(height: 12),
-            Text(
-              'No problems found',
-              style: GoogleFonts.inter(color: context.getColor(ThemeEnum.textSecond), fontSize: 15.r, fontWeight: FontWeight.w600),
-            ),
+            SemiBoldText(StringsManager.noProblemsFound, color: ThemeEnum.textSecond, fontSize: 15),
             const RSizedBox(height: 4),
-            Text('Try a different search or filter',
-                style: GoogleFonts.inter(color: context.getColor(ThemeEnum.hover), fontSize: 13.r)),
+            RegularText(StringsManager.tryADifferentSearchOrFilter, color: ThemeEnum.hover, fontSize: 13),
           ],
         ),
       ),
