ad7ce4ac5db9189671ec46e1b9962dca8899d53f
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 14:56:09 +0300

    add dark theme background accent colors to ColorManager

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -26,8 +26,11 @@ abstract final class ColorManager {
   static const outputHeaderDk = Color.fromRGBO(22, 32, 53, 1);
   static const accentDk = Color.fromRGBO(129, 140, 248, 1);
   static const accentGreenDk = Color.fromRGBO(52, 211, 153, 1);
+  static const accentGreenBgDk = Color.fromRGBO(17, 185, 129, 1);
   static const accentYellowDk = Color.fromRGBO(251, 191, 36, 1);
+  static const accentYellowBgDk = Color.fromRGBO(245, 158, 11, 1);
   static const accentRedDk = Color.fromRGBO(248, 113, 113, 1);
+  static const accentRedBgDk = Color.fromRGBO(239, 68, 68, 1);
   static const accentBlueDk = Color.fromRGBO(96, 165, 250, 1);
   static const textPrimaryDk = Color.fromRGBO(226, 232, 240, 1);
   static const textSecondDk = Color.fromRGBO(148, 163, 184, 1);
