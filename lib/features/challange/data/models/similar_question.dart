9b8f560d097e6c84fe32e13a57e38da3483ac3ba
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 15:01:52 +0300

    make SimilarQuestion fields nullable

--- lib/features/challange/data/models/similar_question.dart
+++ lib/features/challange/data/models/similar_question.dart
@@ -1,4 +1,3 @@
-// lib/models/similar_question.dart
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'similar_question.freezed.dart';
@@ -7,9 +6,9 @@ part 'similar_question.g.dart';
 @freezed
 class SimilarQuestion with _$SimilarQuestion {
   const factory SimilarQuestion({
-    required int problemId,
-    required String name,
-    required String reason,
+    required int? problemId,
+    required String? name,
+    required String? reason,
   }) = _SimilarQuestion;
 
   factory SimilarQuestion.fromJson(Map<String, dynamic> json) => _$SimilarQuestionFromJson(json);
