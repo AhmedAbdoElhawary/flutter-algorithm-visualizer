79e965b1efe68589a3d656193dac84ef09e0e530
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 07:06:10 +0300

    add generated JSON serialization for SolutionApproach

---
+++ lib/features/challange/data/models/solution_approach.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'solution_approach.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SolutionApproachImpl _$$SolutionApproachImplFromJson(
+        Map<String, dynamic> json) =>
+    _$SolutionApproachImpl(
+      keyObservation: json['key_observation'] as String,
+      algorithm: json['algorithm'] as String,
+      whyItWorks: json['why_it_works'] as String,
+      implementationNotes: json['implementation_notes'] as String,
+    );
+
+Map<String, dynamic> _$$SolutionApproachImplToJson(
+        _$SolutionApproachImpl instance) =>
+    <String, dynamic>{
+      'key_observation': instance.keyObservation,
+      'algorithm': instance.algorithm,
+      'why_it_works': instance.whyItWorks,
+      'implementation_notes': instance.implementationNotes,
+    };
