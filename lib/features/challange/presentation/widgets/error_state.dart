5617b51ad052864b7b2f4c52ddb80d30bdb20586
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 16:31:14 +0300

    replace CustomIcon with RegularText in ErrorState and update color to accentRedRc

--- lib/features/challange/presentation/widgets/error_state.dart
+++ lib/features/challange/presentation/widgets/error_state.dart
@@ -1,7 +1,6 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
-import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -11,10 +10,10 @@ class ChallengesErrorState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:Column(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          CustomIcon(Icons.error_outline_rounded, size: 60, color: ThemeEnum.accent),
+          RegularText('ⓘ', fontSize: 40, color: ThemeEnum.accentRedRc),
           const RSizedBox(height: 12),
           SemiBoldText(StringsManager.errorText, color: ThemeEnum.textSecond, fontSize: 15),
           const RSizedBox(height: 4),
