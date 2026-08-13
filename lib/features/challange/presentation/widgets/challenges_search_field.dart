df17410fb88586b00b48b045d03ec81f932e3494
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 14:55:24 +0300

    move challenges_search_field.dart to features/challange/presentation/widgets

--- lib/features/challange/presentation/widgets/challenges_search_field.dart
+++ lib/features/challange/presentation/widgets/challenges_search_field.dart
@@ -1,7 +1,7 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
-import 'package:algorithm_visualizer/features/practice/view_model/challenges_notifier.dart';
+import 'package:algorithm_visualizer/features/challange/presentation/view_model/challenges_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
