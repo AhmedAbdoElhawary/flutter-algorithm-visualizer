fe5ad720a66021885241ff83a3504e7e7f0bab24
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 17:16:31 +0300

    privatize challengeDatasetProvider and update difficulty filtering to handle ProblemDifficulty.none

--- lib/features/challange/presentation/view_model/challenges_providers.dart
+++ lib/features/challange/presentation/view_model/challenges_providers.dart
@@ -13,7 +13,7 @@ final _challengeRepositoryProvider = Provider<ProblemRepository>(
   (ref) => ProblemRepositoryImpl(ProblemLocalDataSource(GetStorageService(GetStorage()))),
 );
 
-final challengeDatasetProvider = FutureProvider<List<CodingProblem>>(
+final _challengeDatasetProvider = FutureProvider<List<CodingProblem>>(
   (ref) {
     final dataSource = ref.read(_challengeRepositoryProvider);
 
@@ -22,14 +22,17 @@ final challengeDatasetProvider = FutureProvider<List<CodingProblem>>(
 );
 
 final problemsProvider = Provider<AsyncValue<List<CodingProblem>>>((ref) {
-  final problemsAsync = ref.watch(challengeDatasetProvider);
+  final problemsAsync = ref.watch(_challengeDatasetProvider);
   final state = ref.watch(challengesProvider);
 
   return problemsAsync.whenData(
     (problems) {
       return problems.where((problem) {
-        if (state.filter != null && problem.difficulty != state.filter) return false;
-
+        if (state.filter != null &&
+            state.filter != ProblemDifficulty.none &&
+            problem.difficulty != state.filter) {
+          return false;
+        }
         if (state.search.isNotEmpty && !problem.getName.toLowerCase().contains(state.search.toLowerCase())) {
           return false;
         }
@@ -39,22 +42,19 @@ final problemsProvider = Provider<AsyncValue<List<CodingProblem>>>((ref) {
   );
 });
 
-/// todo: make it actual data not [problemId]
 final solvedCountProvider = Provider<AsyncValue<int>>((ref) {
-  final problems = ref.watch(challengeDatasetProvider);
+  final problems = ref.watch(_challengeDatasetProvider);
 
   return problems.whenData((problems) => problems.where((problem) => problem.isSolved).length);
 });
 
 final difficultyCountProvider = Provider.family<AsyncValue<int>, ProblemDifficulty?>(
   (ref, filter) {
-    final problems = ref.watch(challengeDatasetProvider);
+    final problems = ref.watch(_challengeDatasetProvider);
 
     return problems.whenData(
       (problems) {
-        if (filter == null) {
-          return problems.length;
-        }
+        if (filter == null || filter == ProblemDifficulty.none) return problems.length;
 
         return problems.where((problem) => problem.difficulty == filter).length;
       },
