d23e39aa6dd7342858ac4b864ce1216437cf3644
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 07:04:27 +0300

    add generated json serialization for Example model

---
+++ lib/features/challange/data/models/example.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ExampleImpl _$$ExampleImplFromJson(Map<String, dynamic> json) =>
+    _$ExampleImpl(
+      input: json['input'] as Map<String, dynamic>,
+      output: json['output'],
+      explanation: json['explanation'] as String,
+    );
+
+Map<String, dynamic> _$$ExampleImplToJson(_$ExampleImpl instance) =>
+    <String, dynamic>{
+      'input': instance.input,
+      'output': instance.output,
+      'explanation': instance.explanation,
+    };
