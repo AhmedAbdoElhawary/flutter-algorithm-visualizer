acad3ce50bd641e0cb864c09dc58450f0182cbb5
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 14:59:43 +0300

    add ProblemStorageDTO and ProblemSolutionStatusDTO to challenge data models

---
+++ lib/features/challange/data/models/problem_storage.dart
@@ -0,0 +1,30 @@
+import 'package:algorithm_visualizer/features/challange/data/models/test_case.dart';
+import 'package:algorithm_visualizer/features/challange/domain/enums/problem.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'problem_storage.freezed.dart';
+part 'problem_storage.g.dart';
+
+@freezed
+class ProblemStorageDTO with _$ProblemStorageDTO {
+  const factory ProblemStorageDTO({
+    required int? problemId,
+    required ProblemStatus? problemStatus,
+    required bool? isBookmarked,
+    required List<ProblemSolutionStatusDTO>? solutionsStatus,
+  }) = _ProblemStorageDTO;
+
+  factory ProblemStorageDTO.fromJson(Map<String, dynamic> json) => _$ProblemStorageDTOFromJson(json);
+}
+
+@freezed
+class ProblemSolutionStatusDTO with _$ProblemSolutionStatusDTO {
+  const factory ProblemSolutionStatusDTO({
+    required String? code,
+    required TestCase? failedTestCase,
+    required bool? isCorrect,
+  }) = _ProblemSolutionStatusDTO;
+
+  factory ProblemSolutionStatusDTO.fromJson(Map<String, dynamic> json) =>
+      _$ProblemSolutionStatusDTOFromJson(json);
+}
