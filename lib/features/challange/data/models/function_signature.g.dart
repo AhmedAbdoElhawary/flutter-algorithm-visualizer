3660a7f2b08d7acddc0f5a406fe20f806fbe4fa3
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-13 07:04:47 +0300

    add generated json serialization for FunctionSignature

---
+++ lib/features/challange/data/models/function_signature.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'function_signature.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$FunctionSignatureImpl _$$FunctionSignatureImplFromJson(
+        Map<String, dynamic> json) =>
+    _$FunctionSignatureImpl(
+      generic: json['generic'] as String,
+      dart: json['dart'] as String,
+    );
+
+Map<String, dynamic> _$$FunctionSignatureImplToJson(
+        _$FunctionSignatureImpl instance) =>
+    <String, dynamic>{
+      'generic': instance.generic,
+      'dart': instance.dart,
+    };
