81ce66bd170c0ca8ea258cb0aafab7a7b916e5a8
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-15 12:38:04 +0300

    update input and output fields to String? in Example model

--- lib/features/challange/data/models/example.dart
+++ lib/features/challange/data/models/example.dart
@@ -6,9 +6,10 @@ part 'example.g.dart';
 @freezed
 class Example with _$Example {
   const factory Example({
-    required Map<String, dynamic>? input,
-    required dynamic output,
-    required String? explanation,
+   required String? input,
+   required String? output,
+   required String? explanation,
+
   }) = _Example;
 
   factory Example.fromJson(Map<String, dynamic> json) => _$ExampleFromJson(json);
