33229587063deccfcd60c2a769847489e973625a
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-15 12:37:26 +0300

    comment out showOutput and loadingTestCases in CodeEditorState

--- lib/features/challange/challange/presentation/providers/code_editor_state.dart
+++ lib/features/challange/challange/presentation/providers/code_editor_state.dart
@@ -9,50 +9,50 @@ const _unset = _Unset();
 class CodeEditorState {
   const CodeEditorState({
     required this.isRunning,
-    required this.showOutput,
+    // required this.showOutput,
     required this.copied,
     required this.highlightedLine,
     // required this.testCases,
-    required this.loadingTestCases,
+    // required this.loadingTestCases,
     // required this.result,
   });
 
   factory CodeEditorState.initial() => const CodeEditorState(
         isRunning: false,
-        showOutput: false,
+        // showOutput: false,
         copied: false,
         highlightedLine: null,
         // testCases: [],
-        loadingTestCases: true,
+        // loadingTestCases: true,
         // result: null,
       );
 
   final bool isRunning;
-  final bool showOutput;
+  // final bool showOutput;
   final bool copied;
   final int? highlightedLine;
   // final List<CodeTestCase> testCases;
-  final bool loadingTestCases;
+  // final bool loadingTestCases;
   // final ExecutionResult? result;
 
   CodeEditorState copyWith({
     bool? isRunning,
-    bool? showOutput,
+    // bool? showOutput,
     bool? copied,
     Object? highlightedLine = _unset,
     // List<CodeTestCase>? testCases,
-    bool? loadingTestCases,
+    // bool? loadingTestCases,
     // Object? result = _unset,
   }) {
     return CodeEditorState(
       isRunning: isRunning ?? this.isRunning,
-      showOutput: showOutput ?? this.showOutput,
+      // showOutput: showOutput ?? this.showOutput,
       copied: copied ?? this.copied,
       highlightedLine: identical(highlightedLine, _unset)
           ? this.highlightedLine
           : highlightedLine as int?,
       // testCases: testCases ?? this.testCases,
-      loadingTestCases: loadingTestCases ?? this.loadingTestCases,
+      // loadingTestCases: loadingTestCases ?? this.loadingTestCases,
       // result: identical(result, _unset) ? this.result : result as ExecutionResult?,
     );
   }
