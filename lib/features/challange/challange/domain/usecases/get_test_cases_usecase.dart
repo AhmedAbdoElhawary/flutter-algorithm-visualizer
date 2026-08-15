c70a73e53f6a25472dbd7e270aa81f83192d67c4
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-15 09:11:50 +0300

    add GetTestCasesUseCase and RunCodeUseCase

---
+++ lib/features/challange/challange/domain/usecases/get_test_cases_usecase.dart
@@ -0,0 +1,12 @@
+// import 'package:algorithm_visualizer/features/challange/challange/domain/entities/test_case.dart';
+// import 'package:algorithm_visualizer/features/challange/challange/domain/repositories/test_case_repository.dart';
+//
+// class GetTestCasesUseCase {
+//   const GetTestCasesUseCase(this._repository);
+//
+//   final TestCaseRepository _repository;
+//
+//   Future<List<CodeTestCase>> call(int problemId) {
+//     return _repository.getTestCases(problemId);
+//   }
+// }
