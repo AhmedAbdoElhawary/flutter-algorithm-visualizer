06f20d579eed33ae0a272068ee3d4dd8ed3d7013
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-15 12:38:31 +0300

    add FunctionSignature data model

---
+++ lib/features/challange/data/models/function_signature.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'function_signature.freezed.dart';
+part 'function_signature.g.dart';
+
+@freezed
+class FunctionSignature with _$FunctionSignature {
+  const factory FunctionSignature({
+    required String? generic,
+    required String? dart,
+
+  }) = _FunctionSignature;
+
+  factory FunctionSignature.fromJson(Map<String, dynamic> json) => _$FunctionSignatureFromJson(json);
+}
