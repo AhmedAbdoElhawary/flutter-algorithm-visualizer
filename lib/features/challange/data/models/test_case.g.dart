c504b35cccfa2d2a9069295f9d54e417e537def3
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-16 17:32:16 +0300

    add SingleTestCaseResult JSON serialization to test_case.g.dart

--- lib/features/challange/data/models/test_case.g.dart
+++ lib/features/challange/data/models/test_case.g.dart
@@ -17,3 +17,23 @@ Map<String, dynamic> _$$TestCaseImplToJson(_$TestCaseImpl instance) =>
       'input': instance.input,
       'expected_output': instance.expectedOutput,
     };
+
+_$SingleTestCaseResultImpl _$$SingleTestCaseResultImplFromJson(
+        Map<String, dynamic> json) =>
+    _$SingleTestCaseResultImpl(
+      input: json['input'] as String?,
+      expectedOutput: json['expected_output'] as String?,
+      actualOutput: json['actual_output'] as String,
+      passed: json['passed'] as bool,
+      errorMessage: json['error_message'] as String?,
+    );
+
+Map<String, dynamic> _$$SingleTestCaseResultImplToJson(
+        _$SingleTestCaseResultImpl instance) =>
+    <String, dynamic>{
+      'input': instance.input,
+      'expected_output': instance.expectedOutput,
+      'actual_output': instance.actualOutput,
+      'passed': instance.passed,
+      'error_message': instance.errorMessage,
+    };
