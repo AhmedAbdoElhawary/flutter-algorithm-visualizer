d162249df883abd365f6c748313e97f50df0ef84
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-16 12:42:55 +0300

    update background and text colors to lightPink and pink in CodeEditorLangBar

--- lib/features/challange/challange/presentation/widgets/code_editor_lang_bar.dart
+++ lib/features/challange/challange/presentation/widgets/code_editor_lang_bar.dart
@@ -39,10 +39,10 @@ class CodeEditorLangBar extends ConsumerWidget {
               child: Container(
                 padding: REdgeInsets.symmetric(horizontal: 14, vertical: 5),
                 decoration: BoxDecoration(
-                  color: context.getColor(ThemeEnum.accentBg),
+                  color: context.getColor(ThemeEnum.lightPink),
                   borderRadius: BorderRadius.circular(7),
                 ),
-                child: SemiBoldText(language, color: ThemeEnum.accent, fontSize: 12),
+                child: SemiBoldText(language, color: ThemeEnum.pink, fontSize: 12),
               ),
             ),
             const Spacer(),
