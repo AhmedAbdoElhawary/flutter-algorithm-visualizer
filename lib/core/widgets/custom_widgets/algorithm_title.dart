c3ff31aa980ae8564bd3dfbfee8fb7544d768311
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-17 09:29:28 +0300

    add top padding and update title font weight in AlgorithmTitle

--- lib/core/widgets/custom_widgets/algorithm_title.dart
+++ lib/core/widgets/custom_widgets/algorithm_title.dart
@@ -1,6 +1,8 @@
+import 'package:algorithm_visualizer/core/resources/font_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AlgorithmTitle extends StatelessWidget {
   const AlgorithmTitle({
@@ -14,18 +16,21 @@ class AlgorithmTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        BoldText(title, fontSize: 20),
-        RegularText(
-          description,
-          fontSize: 11,
-          color: ThemeEnum.hover,
-        )
-      ],
+    return Padding(
+      padding: REdgeInsets.only(top: 5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BoldText(title, fontSize: 20, fontWeight: FontWeightManager.bold900),
+          RegularText(
+            description,
+            fontSize: 11,
+            color: ThemeEnum.hover,
+          )
+        ],
+      ),
     );
   }
 }
