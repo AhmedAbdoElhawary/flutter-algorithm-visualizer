f2611a6eccac06e367cc43e8db0afdbb902d300b
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-17 08:56:31 +0300

    update total count calculation to use filteredProblemIdsProvider in ChallengesHeader

--- lib/features/challange/presentation/widgets/challenges/challenges_header.dart
+++ lib/features/challange/presentation/widgets/challenges/challenges_header.dart
@@ -12,8 +12,9 @@ class ChallengesHeader extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final total =
-        ref.watch(filteredProblemsProvider).maybeWhen(data: (data) => data.length, orElse: () => -1);
+    final total = ref
+        .watch(filteredProblemIdsProvider)
+        .maybeWhen(data: (data) => data.ids.length, orElse: () => -1);
     final totalText = total == -1 ? StringsManager.nan : "$total";
 
     final solved = ref.watch(solvedCountProvider).maybeWhen(data: (data) => data, orElse: () => -1);
