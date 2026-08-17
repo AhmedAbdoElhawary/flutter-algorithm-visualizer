3e5353709705b0fbc768ecc29a5814ee26cfc151
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-17 14:44:53 +0300

    add submittedAt to ProblemSolutionStatusDTO in problem_storage

--- lib/features/challange/data/models/problem_storage.dart
+++ lib/features/challange/data/models/problem_storage.dart
@@ -24,6 +24,7 @@ class ProblemSolutionStatusDTO with _$ProblemSolutionStatusDTO {
     required String? code,
     required List<TestCaseResult>? allTestCaseResults,
     required bool? isCorrect,
+    DateTime? submittedAt,
   }) = _ProblemSolutionStatusDTO;
 
   factory ProblemSolutionStatusDTO.fromJson(Map<String, dynamic> json) =>
