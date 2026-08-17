949f02906af9a8f63efd08d176488586931cebe7
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-17 21:46:52 +0300

    rename recent_submissions_page to practice_history_page and update import to profile_practice_history

--- lib/features/profile/presentation/pages/practice_history_page.dart
+++ lib/features/profile/presentation/pages/practice_history_page.dart
@@ -3,7 +3,7 @@ import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
 import 'package:algorithm_visualizer/features/profile/data/profile_stats_provider.dart';
-import 'package:algorithm_visualizer/features/profile/presentation/widgets/profile_recent_submissions.dart';
+import 'package:algorithm_visualizer/features/profile/presentation/widgets/profile_practice_history.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
