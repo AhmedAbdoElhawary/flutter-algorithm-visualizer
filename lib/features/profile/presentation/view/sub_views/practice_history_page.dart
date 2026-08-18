015605103b7dbef3acc591b99dc252a4aec98dd0
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-18 07:48:38 +0300

    move practice_history_page and bookmarked_problems_page to view/sub_views and update profile_stats_provider import path

--- lib/features/profile/presentation/view/sub_views/practice_history_page.dart
+++ lib/features/profile/presentation/view/sub_views/practice_history_page.dart
@@ -2,7 +2,7 @@ import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
-import 'package:algorithm_visualizer/features/profile/data/profile_stats_provider.dart';
+import 'package:algorithm_visualizer/features/profile/presentation/view_model/profile_stats_provider.dart';
 import 'package:algorithm_visualizer/features/profile/presentation/widgets/profile_practice_history.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
