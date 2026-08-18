186769be38b676c16cbabd22fee5ac84d5168d3b
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-18 19:38:15 +0300

    update json serialization methods in `solution_approach.g.dart` to reference `SolutionApproach` instead of `_$SolutionApproachImpl`

--- lib/features/challange/data/models/solution_approach.g.dart
+++ lib/features/challange/data/models/solution_approach.g.dart
@@ -6,17 +6,15 @@ part of 'solution_approach.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$SolutionApproachImpl _$$SolutionApproachImplFromJson(
-        Map<String, dynamic> json) =>
-    _$SolutionApproachImpl(
+SolutionApproach _$SolutionApproachFromJson(Map<String, dynamic> json) =>
+    SolutionApproach(
       keyObservation: json['key_observation'] as String?,
       algorithm: json['algorithm'] as String?,
       whyItWorks: json['why_it_works'] as String?,
       implementationNotes: json['implementation_notes'] as String?,
     );
 
-Map<String, dynamic> _$$SolutionApproachImplToJson(
-        _$SolutionApproachImpl instance) =>
+Map<String, dynamic> _$SolutionApproachToJson(SolutionApproach instance) =>
     <String, dynamic>{
       'key_observation': instance.keyObservation,
       'algorithm': instance.algorithm,
