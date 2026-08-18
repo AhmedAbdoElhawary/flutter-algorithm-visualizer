c5960eb03fe14c5fc28bd4602a73f4ee552f8ae6
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-18 16:33:39 +0300

    update default profile name to use `StringsManager.anonymous` in `profile_storage`

--- lib/core/storage/profile_storage.dart
+++ lib/core/storage/profile_storage.dart
@@ -1,10 +1,11 @@
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/storage/get_storage_service.dart';
 import 'package:algorithm_visualizer/core/storage/storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
 
 const _profileNameKey = 'profile_name';
-const _defaultName = 'A';
+const _defaultName = StringsManager.anonymous;
 
 final profileStorageProvider = Provider<ProfileStorage>((ref) {
   return ProfileStorage(GetStorageService(GetStorage()));
