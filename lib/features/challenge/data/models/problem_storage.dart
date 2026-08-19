6431beeb2c4bcd07dc99355782e181a6e40e20c2
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-19 18:13:36 +0300

    change challange folder to challenge

---
+++ lib/features/challenge/data/models/problem_storage.dart
@@ -0,0 +1,68 @@
+import 'package:algorithm_visualizer/features/challenge/data/models/test_case.dart';
+import 'package:algorithm_visualizer/features/challenge/domain/enums/problem.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'problem_storage.g.dart';
+
+@JsonSerializable()
+class ProblemStorageDTO {
+  const ProblemStorageDTO({
+    required this.problemId,
+    required this.problemStatus,
+    required this.isBookmarked,
+    required this.solutionsStatus,
+  });
+
+  final int? problemId;
+  final ProblemStatus? problemStatus;
+  final bool? isBookmarked;
+  final List<ProblemSolutionStatusDTO>? solutionsStatus;
+
+  factory ProblemStorageDTO.fromJson(Map<String, dynamic> json) => _$ProblemStorageDTOFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProblemStorageDTOToJson(this);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProblemStorageDTO &&
+          runtimeType == other.runtimeType &&
+          problemId == other.problemId &&
+          problemStatus == other.problemStatus &&
+          isBookmarked == other.isBookmarked;
+
+  @override
+  int get hashCode => Object.hash(runtimeType, problemId, problemStatus, isBookmarked);
+}
+
+@JsonSerializable()
+class ProblemSolutionStatusDTO {
+  const ProblemSolutionStatusDTO({
+    required this.code,
+    required this.allTestCaseResults,
+    required this.isCorrect,
+    this.submittedAt,
+  });
+
+  final String? code;
+  final List<TestCaseResult>? allTestCaseResults;
+  final bool? isCorrect;
+  final DateTime? submittedAt;
+
+  factory ProblemSolutionStatusDTO.fromJson(Map<String, dynamic> json) =>
+      _$ProblemSolutionStatusDTOFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProblemSolutionStatusDTOToJson(this);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProblemSolutionStatusDTO &&
+          runtimeType == other.runtimeType &&
+          code == other.code &&
+          isCorrect == other.isCorrect &&
+          submittedAt == other.submittedAt;
+
+  @override
+  int get hashCode => Object.hash(runtimeType, code, isCorrect, submittedAt);
+}
