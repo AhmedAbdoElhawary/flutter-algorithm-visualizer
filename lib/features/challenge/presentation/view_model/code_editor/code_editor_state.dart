cc4c083c0755ace08db7ef7ce7accaab577da0c7
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-19 18:13:48 +0300

    change challange folder to challenge

---
+++ lib/features/challenge/presentation/view_model/code_editor/code_editor_state.dart
@@ -0,0 +1,32 @@
+import 'package:algorithm_visualizer/features/challenge/domain/usecases/grade_code_usecase.dart';
+
+class CodeEditorState {
+  const CodeEditorState({
+    required this.isRunning,
+    required this.copied,
+    required this.highlightedLine,
+    required this.grade,
+  });
+
+  factory CodeEditorState.initial() =>
+      const CodeEditorState(isRunning: false, copied: false, highlightedLine: null, grade: null);
+
+  final bool isRunning;
+  final bool copied;
+  final int? highlightedLine;
+  final CodeGradeResult? grade;
+
+  CodeEditorState copyWith({
+    bool? isRunning,
+    bool? copied,
+    int? highlightedLine,
+    CodeGradeResult? grade,
+  }) {
+    return CodeEditorState(
+      isRunning: isRunning ?? this.isRunning,
+      copied: copied ?? this.copied,
+      highlightedLine: highlightedLine ?? this.highlightedLine,
+      grade: grade ?? this.grade,
+    );
+  }
+}
