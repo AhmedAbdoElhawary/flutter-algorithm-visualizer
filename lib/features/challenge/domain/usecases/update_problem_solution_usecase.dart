b241c0e97d254b9a69903695808f15f4352be7cb
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-19 18:13:44 +0300

    change challange folder to challenge

---
+++ lib/features/challenge/domain/usecases/update_problem_solution_usecase.dart
@@ -0,0 +1,33 @@
+import 'package:algorithm_visualizer/features/challenge/data/models/problem_storage.dart';
+import 'package:algorithm_visualizer/features/challenge/domain/entities/coding_problem.dart';
+import 'package:algorithm_visualizer/features/challenge/domain/enums/problem.dart';
+import 'package:algorithm_visualizer/features/challenge/domain/repositories/problem_repository.dart';
+
+import 'grade_code_usecase.dart' show CodeGradeResult;
+
+class UpdateProblemSolutionUseCase {
+  final ProblemRepository repository;
+  UpdateProblemSolutionUseCase(this.repository);
+
+  Future<CodingProblem> call(CodingProblem problem, CodeGradeResult result) async {
+    final dto = ProblemStorageDTO.fromJson(problem.toJson());
+
+    final status = ProblemSolutionStatusDTO(
+      code: result.code,
+      allTestCaseResults: result.allTestCaseResults,
+      isCorrect: result.allPassed,
+      submittedAt: DateTime.now(),
+    );
+
+    final solution = dto.solutionsStatus ?? [];
+
+    final updatedProblem = problem.copyWith(
+      problemStatus: result.allPassed ? ProblemStatus.solved : ProblemStatus.attempted,
+      solutionsStatus: [status, ...solution],
+      isBookmarked: null,
+    );
+    await repository.updateProblem(updatedProblem);
+
+    return updatedProblem;
+  }
+}
