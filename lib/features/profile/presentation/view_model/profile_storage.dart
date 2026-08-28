2fbc8381e329e5c614c4470f4453abc2b5a4ea84
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-28 16:11:51 +0300

    convert `profile_storage` to part of `user_provider` and rename `profileStorageProvider` to `profileStorageInstanceProvider`

--- lib/features/profile/presentation/view_model/profile_storage.dart
+++ lib/features/profile/presentation/view_model/profile_storage.dart
@@ -1,13 +1,10 @@
-import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
-import 'package:algorithm_visualizer/core/storage/get_storage_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:get_storage/get_storage.dart';
+part of'user_provider.dart';
 
 /// make it simple and didn't use clean architecture layers, just simple MVVM
 const _profileNameKey = 'profile_name';
 const _defaultName = StringsManager.anonymous;
 
-final profileStorageProvider = Provider<ProfileStorage>((ref) {
+final profileStorageInstanceProvider = Provider<ProfileStorage>((ref) {
   return ProfileStorage(GetStorageService(GetStorage()));
 });
 
