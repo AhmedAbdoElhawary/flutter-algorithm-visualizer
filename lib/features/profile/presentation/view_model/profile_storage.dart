528f03f149a1716a63319e8c66636fa42dad74d4
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-08-28 18:25:16 +0300

    refactor `ProfileStorage` to abstract class, implement `GetProfileStorage`, and add name trimming validation in `user_provider`

--- lib/features/profile/presentation/view_model/profile_storage.dart
+++ lib/features/profile/presentation/view_model/profile_storage.dart
@@ -1,19 +1,26 @@
-part of'user_provider.dart';
+part of 'user_provider.dart';
 
 /// make it simple and didn't use clean architecture layers, just simple MVVM
 const _profileNameKey = 'profile_name';
-const _defaultName = StringsManager.anonymous;
+const defaultName = StringsManager.anonymous;
 
 final profileStorageInstanceProvider = Provider<ProfileStorage>((ref) {
-  return ProfileStorage(GetStorageService(GetStorage()));
+  return GetProfileStorage(GetStorageService(GetStorage()));
 });
 
-class ProfileStorage {
-  ProfileStorage(this._storage);
+abstract class ProfileStorage {
+  String getProfileName();
+  Future<void> saveProfileName(String name);
+}
+
+class GetProfileStorage implements ProfileStorage {
+  GetProfileStorage(this._storage);
 
   final GetStorageService _storage;
 
-  String getProfileName() => _storage.read<String>(_profileNameKey) ?? _defaultName;
+  @override
+  String getProfileName() => _storage.read<String>(_profileNameKey) ?? defaultName;
 
+  @override
   Future<void> saveProfileName(String name) => _storage.write(_profileNameKey, name);
 }
