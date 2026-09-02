b02597f3ee14b0a7578b5aff3125786ab8f4385f
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-02 13:11:11 +0300

    initialize Firebase in `main`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:algorithm_visualizer/core/material_app/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
@@ -6,6 +7,10 @@ import 'package:get_storage/get_storage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  try {
+    await Firebase.initializeApp();
+  } catch (_) {}
 
   runApp(const ProviderScope(child: MyApp()));
 }
+
