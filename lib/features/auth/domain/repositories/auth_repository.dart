e2cbbbccf5caa54e4097504c069ea59dc90945d8
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-02 13:06:00 +0300

    create `AuthRepository` abstract class in `auth` domain

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:algorithm_visualizer/features/auth/domain/entities/auth_user.dart';
+
+abstract class AuthRepository {
+  Future<AuthUser> login({required String email, required String password});
+  Future<AuthUser> register({required String name, required String email, required String password});
+  Future<void> forgotPassword({required String email});
+  Future<void> resetPassword({required String code, required String newPassword});
+  Future<AuthUser?> getCurrentUser();
+  Future<void> logout();
+}
