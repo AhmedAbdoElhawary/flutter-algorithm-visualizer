31a2cb8305f828fb70d590fdf6d155b8a10b9d96
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-02 15:48:55 +0300

    initialize Firebase with `DefaultFirebaseOptions` and parallelize startup in `lib/main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:algorithm_visualizer/core/material_app/my_app.dart';
+import 'package:algorithm_visualizer/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,11 +7,12 @@ import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init();
   try {
-    await Firebase.initializeApp();
+    await Future.wait([
+      GetStorage.init(),
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+    ]);
   } catch (_) {}
 
   runApp(const ProviderScope(child: MyApp()));
 }
-
