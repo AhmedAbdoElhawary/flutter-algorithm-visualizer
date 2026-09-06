3d905a629f18a5098b233f2eec72dfca77fbdb5d
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-06 12:45:21 +0300

    use `currentUserNameProvider` in `home_header.dart`

--- lib/features/home/view/widgets/home_header.dart
+++ lib/features/home/view/widgets/home_header.dart
@@ -13,7 +13,7 @@ class HomeHeader extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // in case i will add others to not rebuild it just to take the name
-    final name = ref.watch(profileStorageProvider.select((value) => value.username));
+    final name = ref.watch(currentUserNameProvider.select((value) => value));
     final greeting = ref.watch(homeDataProvider.select((value) => value.greeting));
 
     return OnlyPadding(
