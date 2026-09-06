d95d3ca197b5de01fa2b90cba35166b508f3b0fd
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-06 12:51:48 +0300

    integrate `ProblemRemoteDataSource` and synchronization logic in `ProblemRepositoryImpl`

--- lib/features/challenge/data/repositories/problem_repository_impl.dart
+++ lib/features/challenge/data/repositories/problem_repository_impl.dart
@@ -1,22 +1,25 @@
 import 'package:algorithm_visualizer/features/challenge/data/data_sources/local/challenge_local_data_source.dart';
+import 'package:algorithm_visualizer/features/challenge/data/data_sources/remote/challenge_remote_data_source.dart';
 import 'package:algorithm_visualizer/features/challenge/data/mappers/problem_mapper.dart';
 import 'package:algorithm_visualizer/features/challenge/data/models/problem_storage.dart';
 import 'package:algorithm_visualizer/features/challenge/domain/entities/coding_problem.dart';
 import 'package:algorithm_visualizer/features/challenge/domain/repositories/problem_repository.dart';
 import 'package:collection/collection.dart';
+import 'package:flutter/cupertino.dart';
 
 class ProblemRepositoryImpl implements ProblemRepository {
-  final ProblemLocalDataSource dataSource;
+  ProblemRepositoryImpl(this.localDataSource, this.remoteDataSource);
 
-  ProblemRepositoryImpl(this.dataSource);
+  final ProblemLocalDataSource localDataSource;
+  final ProblemRemoteDataSource remoteDataSource;
 
   @override
   Future<List<CodingProblem>> getAllProblems() async {
-    final assetsProblems = await dataSource.loadProblemsAssets();
-    final localProblems = dataSource.getProblems();
+    final assetsProblems = await localDataSource.loadProblemsAssets();
+    final storageProblems = await _loadStorageProblems();
 
     final problems = assetsProblems.problems?.map((dto) {
-      final localProblem = localProblems.firstWhereOrNull((lp) => lp.problemId == dto.problemId);
+      final localProblem = storageProblems.firstWhereOrNull((lp) => lp.problemId == dto.problemId);
       return ProblemMapper.toDomain(dto, localProblem);
     }).toList();
 
@@ -26,17 +29,49 @@ class ProblemRepositoryImpl implements ProblemRepository {
   @override
   Future<void> saveProblem(CodingProblem problem) async {
     final dto = ProblemStorageDTO.fromJson(problem.toJson());
-    await dataSource.saveProblem(dto);
+
+    await Future.wait([
+      localDataSource.saveProblem(dto),
+      _tryRemote(() => remoteDataSource.saveProblem(dto)),
+    ]);
   }
 
   @override
   Future<void> updateProblem(CodingProblem problem) async {
     final dto = ProblemStorageDTO.fromJson(problem.toJson());
-    await dataSource.updateProblem(dto);
+
+    await Future.wait([
+      localDataSource.updateProblem(dto),
+      _tryRemote(() => remoteDataSource.updateProblem(dto)),
+    ]);
   }
 
   @override
   Future<void> deleteProblem(int problemId) async {
-    await dataSource.deleteProblem(problemId);
+    await Future.wait([
+      localDataSource.deleteProblem(problemId),
+      _tryRemote(() => remoteDataSource.deleteProblem(problemId)),
+    ]);
+  }
+
+  Future<List<ProblemStorageDTO>> _loadStorageProblems() async {
+    if (!remoteDataSource.isSignedIn) return localDataSource.getProblems();
+
+    try {
+      final remoteProblems = await remoteDataSource.getProblems();
+      await localDataSource.overwriteProblems(remoteProblems);
+      return remoteProblems;
+    } catch (_) {
+      return localDataSource.getProblems();
+    }
+  }
+
+  Future<void> _tryRemote(Future<void> Function() action) async {
+    if (!remoteDataSource.isSignedIn) return;
+    try {
+      await action();
+    } catch (e) {
+      debugPrint("Something went wrong: $e");
+    }
   }
 }
