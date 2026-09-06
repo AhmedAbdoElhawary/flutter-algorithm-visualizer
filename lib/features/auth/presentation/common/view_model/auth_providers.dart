383f2c9c7441f0b123786456ef15dd1cc9dc78c8
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-06 20:53:41 +0300

    wire `localStorageProvider` and add `guestDataServiceProvider` in `auth_providers.dart`

--- lib/features/auth/presentation/common/view_model/auth_providers.dart
+++ lib/features/auth/presentation/common/view_model/auth_providers.dart
@@ -1,13 +1,15 @@
-import 'package:algorithm_visualizer/core/storage/get_storage_service.dart';
+import 'package:algorithm_visualizer/core/storage/storage_providers.dart';
 import 'package:algorithm_visualizer/features/auth/data/data_sources/local/auth_local_data_source.dart';
 import 'package:algorithm_visualizer/features/auth/data/data_sources/remote/auth_remote_data_source.dart';
 import 'package:algorithm_visualizer/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:algorithm_visualizer/features/auth/domain/repositories/auth_repository.dart';
+import 'package:algorithm_visualizer/features/auth/domain/services/guest_data_service.dart';
+import 'package:algorithm_visualizer/features/challenge/presentation/view_model/challenges/problems_providers.dart';
+import 'package:algorithm_visualizer/features/profile/presentation/view_model/user_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:get_storage/get_storage.dart';
 
 final authLocalDataSourceProvider = Provider<AuthLocalDataSource>((ref) {
-  return AuthLocalDataSourceImpl(GetStorageService(GetStorage()));
+  return AuthLocalDataSourceImpl(ref.watch(localStorageProvider));
 });
 
 final authRemoteDataSourceProvider =
@@ -17,4 +19,13 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
   final local = ref.watch(authLocalDataSourceProvider);
   final remote = ref.watch(authRemoteDataSourceProvider);
   return AuthRepositoryImpl(localDataSource: local, remoteDataSource: remote);
+});
+
+final guestDataServiceProvider = Provider<GuestDataService>((ref) {
+  return GuestDataService(
+    problemLocalDataSource: ref.watch(problemLocalDataSourceProvider),
+    problemRemoteDataSource: ref.watch(problemRemoteDataSourceProvider),
+    profileLocalDataSource: ref.watch(profileLocalDataSourceProvider),
+    storage: ref.watch(localStorageProvider),
+  );
 });
