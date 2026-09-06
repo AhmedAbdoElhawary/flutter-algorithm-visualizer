21f1c3f3b2dcf19802478231c5ca3c3b5dd29382
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-06 16:40:15 +0300

    update `currentUserProvider` and `currentUserNameProvider` for `AsyncValue` state

--- lib/features/profile/presentation/view_model/user_provider.dart
+++ lib/features/profile/presentation/view_model/user_provider.dart
@@ -1,10 +1,10 @@
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/features/auth/domain/entities/auth_user.dart';
 import 'package:algorithm_visualizer/features/auth/presentation/common/view_model/auth_providers.dart';
 import 'package:algorithm_visualizer/features/profile/data/data_sources/remote/profile_remote_data_source.dart';
 import 'package:algorithm_visualizer/features/profile/data/repositories/profile_repository_impl.dart';
 import 'package:algorithm_visualizer/features/profile/domain/repositories/profile_repository.dart';
 import 'package:algorithm_visualizer/features/profile/presentation/view_model/profile_notifier.dart';
-import 'package:algorithm_visualizer/features/profile/presentation/view_model/profile_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final profileRemoteDataSourceProvider =
@@ -16,14 +16,15 @@ final profileRepositoryProvider = Provider<ProfileRepository>((ref) {
   return ProfileRepositoryImpl(localDataSource: local, remoteDataSource: remote);
 });
 
-final profileProvider = NotifierProvider.autoDispose<ProfileNotifier, ProfileState>(() {
+final profileProvider = NotifierProvider.autoDispose<ProfileNotifier, AsyncValue<AuthUser?>>(() {
   return ProfileNotifier();
 });
 
-final currentUserProvider = Provider<AuthUser?>((ref) {
-  return ref.watch(profileProvider.select((state) => state.user));
+final currentUserProvider = Provider<AsyncValue<AuthUser?>>((ref) {
+  return ref.watch(profileProvider);
 });
 
-final currentUserNameProvider = Provider<String>((ref) {
-  return ref.watch(profileProvider.select((state) => (state.user?.name ?? state.newDisplayName)));
+final currentUserNameProvider = Provider<AsyncValue<String>>((ref) {
+  return ref.watch(profileProvider
+      .select((state) => (state.whenData((value) => value?.name ?? StringsManager.anonymous))));
 });
