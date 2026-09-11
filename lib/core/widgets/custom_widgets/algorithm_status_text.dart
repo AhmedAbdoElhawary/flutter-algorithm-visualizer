0519779734f48b4257dad2bd7e7a94f8f13023b4
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-11 15:47:08 +0300

    refactor: rebuild the algorithm status card on GlassContainer

--- lib/core/widgets/custom_widgets/algorithm_status_text.dart
+++ lib/core/widgets/custom_widgets/algorithm_status_text.dart
@@ -1,6 +1,7 @@
 import 'package:algorithm_visualizer/core/resources/font_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
+import 'package:algorithm_visualizer/core/widgets/custom_widgets/glass_card.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/widgets/linear_progress_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,15 +20,9 @@ class AlgorithmStatusText extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14),
-          border: Border.all(color: context.getColor(ThemeEnum.border)),
-          boxShadow: context.cardShadow,
-        ),
+      padding: REdgeInsets.fromLTRB(16, 0, 16, 0),
+      child: GlassContainer(
+        depth: GlassDepth.recessed,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
