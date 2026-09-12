4199b1ede8b887cb991f7908c24030eb2f948abd
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-12 23:08:35 +0300

    chore: defer heap/shell/radix/counting/bucket sort cards until after MVP

--- lib/features/base/view_model/base_view_model.dart
+++ lib/features/base/view_model/base_view_model.dart
@@ -1,21 +1,26 @@
 import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
-import 'package:algorithm_visualizer/core/widgets/custom_widgets/glass_card.dart';
+import 'package:algorithm_visualizer/core/widgets/custom_widgets/card_container.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/searching/view_model/searching_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sorting_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/bubble_sort_notifier.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/bucket_sort_notifier.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/counting_sort_notifier.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/heap_sort_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/insertion_sort_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/merge_sort_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/quick_sort_notifier.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/radix_sort_notifier.dart';
 import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/selection_sort_notifier.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sub_sorting/shell_sort_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
 
-enum SortingAlgoCards { bubble, selection, insertion, merge, quick, radix, heap, shell, counting, bucket }
+enum SortingAlgoCards {
+  bubble,
+  selection,
+  insertion,
+  merge,
+  quick,
+
+  /// TODO: after MVP
+
+  // radix, heap, shell, counting, bucket
+}
 
 enum SearchingAlgoCards { bfs, dfs, aStar }
 
@@ -104,66 +109,68 @@ class BaseViewModel {
             icon: Icons.waves,
           ),
         );
-      case SortingAlgoCards.heap:
-        return AlgoSortingCard(
-          page: SortingAlgoCards.heap,
-          title: StringsManager.heapSort,
-          instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
-            () => HeapSortNotifier(),
-          ),
-          card: AlgorithmGlassCard(
-            algoComplexity: HeapSortNotifier.algorithmComplexity,
-            icon: Icons.wifi_tethering,
-          ),
-        );
-      case SortingAlgoCards.shell:
-        return AlgoSortingCard(
-          page: SortingAlgoCards.shell,
-          title: StringsManager.shellSort,
-          instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
-            () => ShellSortNotifier(),
-          ),
-          card: AlgorithmGlassCard(
-            algoComplexity: ShellSortNotifier.algorithmComplexity,
-            icon: Icons.blur_linear,
-          ),
-        );
-      case SortingAlgoCards.radix:
-        return AlgoSortingCard(
-          page: SortingAlgoCards.radix,
-          title: StringsManager.radixSort,
-          instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
-            () => RadixSortNotifier(),
-          ),
-          card: AlgorithmGlassCard(
-            algoComplexity: RadixSortNotifier.algorithmComplexity,
-            icon: Icons.pin,
-          ),
-        );
-      case SortingAlgoCards.counting:
-        return AlgoSortingCard(
-          page: SortingAlgoCards.counting,
-          title: StringsManager.countingSort,
-          instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
-            () => CountingSortNotifier(),
-          ),
-          card: AlgorithmGlassCard(
-            algoComplexity: CountingSortNotifier.algorithmComplexity,
-            icon: Icons.format_list_numbered,
-          ),
-        );
-      case SortingAlgoCards.bucket:
-        return AlgoSortingCard(
-          page: SortingAlgoCards.bucket,
-          title: StringsManager.bucketSort,
-          instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
-            () => BucketSortNotifier(),
-          ),
-          card: AlgorithmGlassCard(
-            algoComplexity: BucketSortNotifier.algorithmComplexity,
-            icon: Icons.inventory_2,
-          ),
-        );
+
+      /// TODO: after MVP
+      // case SortingAlgoCards.heap:
+      //   return AlgoSortingCard(
+      //     page: SortingAlgoCards.heap,
+      //     title: StringsManager.heapSort,
+      //     instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
+      //       () => HeapSortNotifier(),
+      //     ),
+      //     card: AlgorithmGlassCard(
+      //       algoComplexity: HeapSortNotifier.algorithmComplexity,
+      //       icon: Icons.wifi_tethering,
+      //     ),
+      //   );
+      // case SortingAlgoCards.shell:
+      //   return AlgoSortingCard(
+      //     page: SortingAlgoCards.shell,
+      //     title: StringsManager.shellSort,
+      //     instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
+      //       () => ShellSortNotifier(),
+      //     ),
+      //     card: AlgorithmGlassCard(
+      //       algoComplexity: ShellSortNotifier.algorithmComplexity,
+      //       icon: Icons.blur_linear,
+      //     ),
+      //   );
+      // case SortingAlgoCards.radix:
+      //   return AlgoSortingCard(
+      //     page: SortingAlgoCards.radix,
+      //     title: StringsManager.radixSort,
+      //     instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
+      //       () => RadixSortNotifier(),
+      //     ),
+      //     card: AlgorithmGlassCard(
+      //       algoComplexity: RadixSortNotifier.algorithmComplexity,
+      //       icon: Icons.pin,
+      //     ),
+      //   );
+      // case SortingAlgoCards.counting:
+      //   return AlgoSortingCard(
+      //     page: SortingAlgoCards.counting,
+      //     title: StringsManager.countingSort,
+      //     instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
+      //       () => CountingSortNotifier(),
+      //     ),
+      //     card: AlgorithmGlassCard(
+      //       algoComplexity: CountingSortNotifier.algorithmComplexity,
+      //       icon: Icons.format_list_numbered,
+      //     ),
+      //   );
+      // case SortingAlgoCards.bucket:
+      //   return AlgoSortingCard(
+      //     page: SortingAlgoCards.bucket,
+      //     title: StringsManager.bucketSort,
+      //     instance: NotifierProvider<SortingNotifier, SortingNotifierState>(
+      //       () => BucketSortNotifier(),
+      //     ),
+      //     card: AlgorithmGlassCard(
+      //       algoComplexity: BucketSortNotifier.algorithmComplexity,
+      //       icon: Icons.inventory_2,
+      //     ),
+      //   );
     }
   }
 
