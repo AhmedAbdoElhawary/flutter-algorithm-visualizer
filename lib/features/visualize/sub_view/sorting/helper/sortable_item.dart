8e669a4d6422caf5f2da6a8b9ad871205d28fe2a
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-13 21:05:58 +0300

    drop per-item sortedStatus in favor of the SortRole-per-index model
    SortableItem no longer carries its own color/status; SortingResult now holds SortStep steps

--- lib/features/visualize/sub_view/sorting/helper/sortable_item.dart
+++ lib/features/visualize/sub_view/sorting/helper/sortable_item.dart
@@ -4,79 +4,25 @@ part of '../view_model/sorting_notifier.dart';
 class SortableItem {
   final int id;
   final int value;
-  final SortingStatus sortedStatus;
-  SortableItem({
-    required this.id,
-    required this.value,
-    this.sortedStatus = SortingStatus.none,
-  });
+  SortableItem({required this.id, required this.value});
 
-  SortableItem copyWith({int? id, int? value, SortingStatus? sortedStatus}) {
-    return SortableItem(
-      id: id ?? this.id,
-      value: value ?? this.value,
-      sortedStatus: sortedStatus ?? this.sortedStatus,
-    );
-  }
-
-  ThemeEnum get getColor {
-    switch (sortedStatus) {
-      case SortingStatus.sorted:
-        return SortingNotifier.doneSortingColor;
-      case SortingStatus.swapping:
-        return SortingNotifier.swappingColor;
-      case SortingStatus.compared:
-        return SortingNotifier.comparedColor;
-      case SortingStatus.temporary:
-        return SortingNotifier.temporaryColor;
-      case SortingStatus.none:
-        return SortingNotifier.itemColor;
-    }
+  SortableItem copyWith({int? id, int? value}) {
+    return SortableItem(id: id ?? this.id, value: value ?? this.value);
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is SortableItem &&
-        other.id == id &&
-        other.value == value &&
-        other.sortedStatus.name == sortedStatus.name;
+    return other is SortableItem && other.id == id && other.value == value;
   }
 
   @override
-  int get hashCode => id.hashCode ^ value.hashCode ^ sortedStatus.name.hashCode;
-}
-
-class SortingStep {
-  final int index1;
-  final int index2;
-  final SortingStatus action;
-
-  SortingStep({
-    required this.index1,
-    required this.index2,
-    required this.action,
-  });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is SortingStep && index1 == other.index1 && index2 == other.index2 && action == other.action;
-
-  @override
-  int get hashCode => Object.hash(index1, index2, action);
-
-  static SortingStep noneStep() => SortingStep(index1: -1, index2: -2, action: SortingStatus.none);
-  Map<String, String> get toMap => {
-        'index1': index1.toString(),
-        'index2': index2.toString(),
-        'action': action.name,
-      };
+  int get hashCode => id.hashCode ^ value.hashCode;
 }
 
 class SortingResult {
-  final List<SortingStep> steps;
+  final List<SortStep> steps;
   final List<int> sortedValues;
 
   SortingResult({required this.steps, required this.sortedValues});
