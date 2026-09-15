0590d426687e12fbb9988f632d7b6b9f5badae5c
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-15 18:04:22 +0300

    switch default text color to the renamed inkTitle token

--- lib/core/widgets/adaptive/text/semi_bold_text.dart
+++ lib/core/widgets/adaptive/text/semi_bold_text.dart
@@ -6,7 +6,7 @@ class SemiBoldText extends _AdaptiveText {
     super.fontSize = 16,
     super.decoration = TextDecoration.none,
     super.fontStyle = FontStyle.normal,
-    super.color = ThemeEnum.focus,
+    super.color = ThemeEnum.inkTitle,
     super.maxLines = 2,
     super.shadows,
     super.textAlign,
