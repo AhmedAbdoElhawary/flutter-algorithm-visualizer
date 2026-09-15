e10fd97dc538e3a147b679c04114680153cdc9c6
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-16 02:18:55 +0300

    add terms and privacy widget

---
+++ lib/core/widgets/custom_widgets/terms_and_privacy.dart
@@ -0,0 +1,64 @@
+import 'package:algorithm_visualizer/core/helpers/constants.dart';
+import 'package:algorithm_visualizer/core/helpers/link_launcher.dart';
+import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
+import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
+import 'package:algorithm_visualizer/core/widgets/adaptive/padding/adaptive_padding.dart';
+import 'package:algorithm_visualizer/core/widgets/adaptive/text/adaptive_text.dart';
+import 'package:flutter/material.dart';
+
+class LegalConsent extends StatelessWidget {
+  const LegalConsent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OnlyPadding(
+      topPadding: 12,
+      bottomPadding: 16,
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        spacing: 4,
+        runSpacing: 2,
+        children: [
+          RegularText(
+            StringsManager.signUpConsentPrefix,
+            color: ThemeEnum.inkMuted,
+            fontSize: 11,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          ),
+          _ConsentLink(label: StringsManager.termsOfService, url: kTermsOfServiceUrl),
+          RegularText(
+            StringsManager.signUpConsentAnd,
+            color: ThemeEnum.inkMuted,
+            fontSize: 11,
+            maxLines: 1,
+          ),
+          _ConsentLink(label: StringsManager.privacyPolicy, url: kPrivacyPolicyUrl),
+        ],
+      ),
+    );
+  }
+}
+
+class _ConsentLink extends StatelessWidget {
+  const _ConsentLink({required this.label, required this.url});
+
+  final String label;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => context.openLink(url),
+      child: MediumText(
+        label,
+        color: ThemeEnum.inkTitle,
+        fontSize: 11,
+        maxLines: 1,
+        decoration: TextDecoration.underline,
+      ),
+    );
+  }
+}
