9ce58337ee7f4929f5de559c7f9d4bd3d162f952
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-16 01:47:17 +0300

    add the delete account provider

---
+++ lib/features/auth/presentation/delete_account/view_model/delete_account_provider.dart
@@ -0,0 +1,8 @@
+import 'package:algorithm_visualizer/features/auth/presentation/delete_account/view_model/delete_account_notifier.dart';
+import 'package:algorithm_visualizer/features/auth/presentation/delete_account/view_model/delete_account_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final deleteAccountProvider =
+    NotifierProvider.autoDispose<DeleteAccountNotifier, DeleteAccountState>(() {
+  return DeleteAccountNotifier();
+});
