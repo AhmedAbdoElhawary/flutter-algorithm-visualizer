c015284fa76a36b488c633923ed7c058f8eacaf6
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-16 20:22:05 +0300

    reformat delete_account_provider.dart

--- lib/features/auth/presentation/delete_account/view_model/delete_account_provider.dart
+++ lib/features/auth/presentation/delete_account/view_model/delete_account_provider.dart
@@ -2,7 +2,6 @@ import 'package:algorithm_visualizer/features/auth/presentation/delete_account/v
 import 'package:algorithm_visualizer/features/auth/presentation/delete_account/view_model/delete_account_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final deleteAccountProvider =
-    NotifierProvider.autoDispose<DeleteAccountNotifier, DeleteAccountState>(() {
+final deleteAccountProvider = NotifierProvider.autoDispose<DeleteAccountNotifier, DeleteAccountState>(() {
   return DeleteAccountNotifier();
 });
