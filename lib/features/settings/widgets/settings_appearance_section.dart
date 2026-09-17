f6e2e0e81a4413ace7540056bea38b6847d5f626
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-17 19:21:10 +0300

    move the settings appearance section out of presentation

--- lib/features/settings/widgets/settings_appearance_section.dart
+++ lib/features/settings/widgets/settings_appearance_section.dart
@@ -3,7 +3,7 @@ import 'package:algorithm_visualizer/core/resources/strings_manager.dart';
 import 'package:algorithm_visualizer/core/resources/theme_manager.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/card_container.dart';
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/custom_icon.dart';
-import 'package:algorithm_visualizer/features/settings/presentation/widgets/settings_row.dart';
+import 'package:algorithm_visualizer/features/settings/widgets/settings_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
