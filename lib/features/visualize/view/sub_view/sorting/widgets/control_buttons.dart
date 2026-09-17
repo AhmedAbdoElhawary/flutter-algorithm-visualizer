c358660f3e26a2a7c583230a35768005eb2916dd
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-17 19:21:13 +0300

    move the sorting control buttons under the visualize view layer

--- lib/features/visualize/view/sub_view/sorting/widgets/control_buttons.dart
+++ lib/features/visualize/view/sub_view/sorting/widgets/control_buttons.dart
@@ -1,5 +1,5 @@
 import 'package:algorithm_visualizer/core/widgets/custom_widgets/algorithm_control.dart';
-import 'package:algorithm_visualizer/features/visualize/sub_view/sorting/view_model/sorting_notifier.dart';
+import 'package:algorithm_visualizer/features/visualize/view/sub_view/sorting/view_model/sorting_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
