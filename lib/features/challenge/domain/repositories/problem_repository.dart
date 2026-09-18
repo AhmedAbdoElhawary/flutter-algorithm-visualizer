9c663ed9977b1d52ed76553b071d1a924f42057e
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-18 06:17:28 +0300

    drop the force remote flag from the problem repository

--- lib/features/challenge/domain/repositories/problem_repository.dart
+++ lib/features/challenge/domain/repositories/problem_repository.dart
@@ -1,7 +1,7 @@
 import 'package:algorithm_visualizer/features/challenge/domain/entities/coding_problem.dart';
 
 abstract class ProblemRepository {
-  Future<List<CodingProblem>> getAllProblems({bool arabic = false, bool forceRemote = false});
+  Future<List<CodingProblem>> getAllProblems({bool arabic = false});
   Future<void> saveProblem(CodingProblem problem);
   Future<void> updateProblem(CodingProblem problem);
   Future<void> deleteProblem(int problemId);
