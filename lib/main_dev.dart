9df2b3ec9298dd8c8b49ecd7b1825977c35433cb
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-21 23:31:39 +0300

    correct the dev entry point comment

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,7 +1,7 @@
 import 'package:algorithm_visualizer/bootstrap.dart';
 import 'package:algorithm_visualizer/core/flavor/flavor_config.dart';
 
-/// Development entry point.
+/// development entry point.
 ///
-/// Run: `flutter run --flavor dev -t lib/main_dev.dart --dart-define-from-file=dart_define/dev.json`
+/// run: `flutter run --flavor dev -t lib/main_dev.dart --dart-define-from-file=dart_define/dev.json`
 Future<void> main() => bootstrap(FlavorConfig.fromEnvironment());
