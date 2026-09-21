e4471f771adaaaabfc6b5607b482eebc40c22818
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2026-09-21 23:31:39 +0300

    correct the production entry point comment

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,7 +1,7 @@
 import 'package:algorithm_visualizer/bootstrap.dart';
 import 'package:algorithm_visualizer/core/flavor/flavor_config.dart';
 
-/// Production entry point.
+/// production entry point.
 ///
-/// Run: `flutter run --flavor production -t lib/main_prod.dart --dart-define-from-file=dart_define/prod.json`
+/// run: `flutter run --flavor production -t lib/main_prod.dart --dart-define-from-file=dart_define/prod.json`
 Future<void> main() => bootstrap(FlavorConfig.fromEnvironment());
